ddc6917edc143432dece5f972117853ab4e3adb7
Jan Zarsky <jan.zarsky@gmail.com>
2020-04-10 14:32:52 +0200

    Refactor packages

--- frontend/src/Packages.js
+++ frontend/src/Packages.js
@@ -8,84 +8,72 @@ class Packages extends React.Component {
     super(props);
     ['name', 'color', 'nameEdit', 'colorEdit'].forEach((field) => this[field] = React.createRef());
     this.handleSubmit = this.handleSubmit.bind(this);
-    this.state = {
-      edit: undefined,
-    };
+    this.state = { edit: undefined };
   }
 
   render() {
     const pkgs = this.props.pkgs;
     var cnt = 0;
 
-    return (
-      <Form onSubmit={this.handleSubmit}>
-        <Table bordered hover responsive>
-          <thead>
-            <tr>
-              <th>#</th>
-              <th>Balíček</th>
-              <th>Barva</th>
-              <th>Akce</th>
-            </tr>
-          </thead>
-          <tbody>
-            {Object.keys(pkgs).map((key) =>
-              <tr key={key}>
-                <td>{cnt += 1}</td>
-                <td>
-                  {(this.state.edit === key) ? (
-                      <Form.Control ref={this.nameEdit} defaultValue={pkgs[key].name} />
-                  ): pkgs[key].name}
-                </td>
-                <td>
-                  {(this.state.edit === key) ? (
-                    <Form.Control ref={this.colorEdit} defaultValue={pkgs[key].color} />
-                  ) : (
-                    <span>
-                      <span className="color-sample"
-                        style={{ backgroundColor: pkgs[key].color }}>&nbsp;</span>
-                      {pkgs[key].color}
-                    </span>
-                  )}
-                </td>
-                <td>
-                  {(this.state.edit === key) ? (
-                    <Button variant="primary" type="submit">
-                      <i className="fa fa-check"></i> Uložit
+    return <Form onSubmit={this.handleSubmit}>
+      <Table bordered hover responsive>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Balíček</th>
+            <th>Barva</th>
+            <th>Akce</th>
+          </tr>
+        </thead>
+        <tbody>
+          {Object.entries(pkgs).map(([key, pkg]) =>
+            <tr key={key}>
+              <td>{cnt += 1}</td>
+              <td>
+                {(this.state.edit === key)
+                  ? <Form.Control ref={this.nameEdit} defaultValue={pkg.name} />
+                  : pkg.name
+                }
+              </td>
+              <td>
+                {(this.state.edit === key)
+                  ? <Form.Control ref={this.colorEdit} defaultValue={pkg.color} />
+                  : <><span className="color-sample"
+                      style={{ backgroundColor: pkg.color }}>&nbsp;</span> {pkg.color}</>
+                }
+              </td>
+              <td>
+                {(this.state.edit === key)
+                  ? <Button variant="primary" type="submit">
+                      <i className="fa fa-check"/> Uložit
                     </Button>
-                  ) : (
-                    <span>
+                  : <span>
                       <Button variant="outline-primary"
                         onClick={() => this.setState({ edit: key })}>
-                        <i className="fa fa-pencil"></i> Upravit
+                        <i className="fa fa-pencil"/> Upravit
                       </Button>&nbsp;
                       <Button variant="outline-danger"
-                        onClick={() => this.props.deletePkg(pkgs[key]._id)}>
-                        <i className="fa fa-trash"></i> Smazat
+                        onClick={() => this.props.deletePkg(pkg._id)}>
+                        <i className="fa fa-trash"/> Smazat
                       </Button>
                     </span>
-                  )}
-                </td>
-              </tr>
-            )}
-            <tr key="new_pkg">
-              <td></td>
-              <td>
-                <Form.Control ref={this.name} defaultValue="Nový balíček" />
-              </td>
-              <td>
-                <Form.Control ref={this.color} defaultValue="#81d4fa" />
-              </td>
-              <td>
-                <Button variant="success" type="submit">
-                  <i className="fa fa-plus"></i> Přidat
-                </Button>
+                }
               </td>
             </tr>
-          </tbody>
-        </Table>
-      </Form>
-    );
+          )}
+          <tr key="new_pkg">
+            <td></td>
+            <td><Form.Control ref={this.name} defaultValue="Nový balíček"/></td>
+            <td><Form.Control ref={this.color} defaultValue="#81d4fa"/></td>
+            <td>
+              <Button variant="success" type="submit">
+                <i className="fa fa-plus"/> Přidat
+              </Button>
+            </td>
+          </tr>
+        </tbody>
+      </Table>
+    </Form>;
   }
 
   handleSubmit(event) {
@@ -93,11 +81,11 @@ class Packages extends React.Component {
 
     if (this.state.edit) {
       this.props.updatePkg({
-        _id: this.state.edit,
-        name: this.nameEdit.current.value,
-        color: this.colorEdit.current.value,
-      })
-      .then(() => this.setState({ edit: undefined }));
+          _id: this.state.edit,
+          name: this.nameEdit.current.value,
+          color: this.colorEdit.current.value,
+        })
+        .then(() => this.setState({ edit: undefined }));
     } else {
       this.props.addPkg({
         name: this.name.current.value,
