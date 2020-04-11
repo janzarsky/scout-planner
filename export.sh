c9d7c99a548b4dabef1fa4f3edf7b341a0813ea0
Jan Zarsky <jan.zarsky@gmail.com>
2020-04-11 20:57:44 +0200

    Fix export

--- export.sh
+++ export.sh
@@ -4,4 +4,5 @@ zip -r xzarsk03.zip backend frontend README.md docker-compose.yml \
     -x "backend/node_modules/*" \
     -x backend/.gitignore \
     -x "frontend/node_modules/*" \
+    -x "frontend/build/*" \
     -x frontend/.gitignore
