c117eb576ef89a5730cf119b6d84b4afd7d7f33c
Vojta Staněk <stanekv01@gmail.com>
2021-01-14 22:53:24 +0100

    Add homepage

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,17 +8,46 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
-import {BrowserRouter, Route, Switch, Redirect, useParams} from 'react-router-dom';
+import {BrowserRouter, Route, Switch, Redirect, useParams, useHistory} from 'react-router-dom';
+import Jumbotron from "react-bootstrap/Jumbotron";
+import Form from "react-bootstrap/Form";
+import InputGroup from "react-bootstrap/InputGroup";
+import Button from "react-bootstrap/Button";
 
 function AppWrapper(props) {
   let { table } = useParams();
   return <App table={table} />
 }
 
+const Homepage = () => {
+    const history = useHistory()
+    const [state, setState] = React.useState('');
+    const submit = React.useCallback(() => {
+        history.push(`/${state}`)
+    }, [history, state])
+    const valid = state.match(/^[\w-]+$/)
+
+    return <Jumbotron className="text-center min-vh-100">
+        <div class="container">
+        <h1 className="mb-5">Skautský plánovač</h1>
+
+        <InputGroup className="mt-3">
+            <Form.Control value={state} onChange={e => setState(e.target.value)} placeholder="Kód" />
+            <InputGroup.Append>
+                <Button disabled={!valid} variant="primary" onClick={submit}>Otevřít</Button>
+            </InputGroup.Append>
+        </InputGroup>
+        </div>
+
+    </Jumbotron>
+}
+
 ReactDOM.render((
   <BrowserRouter>
     <Switch>
-      <Redirect exact from="/" to={"/" + require('crypto').randomBytes(10).toString('hex')} />
+      <Route path="/" exact>
+        <Homepage />
+      </Route>
       <Route path="/:table">
         <AppWrapper />
       </Route>
