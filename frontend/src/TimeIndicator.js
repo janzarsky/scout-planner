1a520fae887019ab8f8e6131b2b181225514b951
Jan Zarsky <jan.zarsky@gmail.com>
2021-10-03 15:53:40 +0200

    Add current time indicator

---
+++ frontend/src/TimeIndicator.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class TimeIndicator extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <div
+      className="timeindicator"
+      style={{
+        gridColumnStart: this.props.rect.x + 3,
+        gridRowStart: this.props.rect.y + 2,
+        gridColumnEnd: 'span ' + this.props.rect.width,
+        gridRowEnd: 'span ' + this.props.rect.height,
+      }}
+    ></div>
+  }
+}
+
+export default TimeIndicator;
