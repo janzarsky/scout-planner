e216a8ad6fde087e360cae5f1e2d8d08f0a29786
Jan Zarsky <jan.zarsky@gmail.com>
2022-03-20 18:24:55 +0100

    Remove old files

--- export.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-zip -r xzarsk03.zip backend frontend README.md docker-compose.yml \
-    -x "backend/node_modules/*" \
-    -x backend/.gitignore \
-    -x "frontend/node_modules/*" \
-    -x "frontend/build/*" \
-    -x frontend/.gitignore
