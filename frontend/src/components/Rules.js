fdee44d649e4ce9cb4bbab3a8a01eb5cfd31b1c0
Jan Zarsky <jan.zarsky@gmail.com>
2022-03-28 21:34:18 +0200

    Refactor Rules

--- frontend/src/components/Rules.js
+++ frontend/src/components/Rules.js
@@ -14,239 +14,262 @@ import {
 export default class Settings extends React.Component {
   constructor(props) {
     super(props);
-    ["program", "condition", "time", "date", "program2"].forEach(
-      (field) => (this[field] = React.createRef())
-    );
+    [
+      "firstProgramRef",
+      "conditionRef",
+      "timeRef",
+      "dateRef",
+      "secondProgramRef",
+    ].forEach((field) => (this[field] = React.createRef()));
     this.handleSubmit = this.handleSubmit.bind(this);
     this.state = { condition: "is_before_date" };
   }
 
   render() {
-    const rules = this.props.rules;
-    const violations = this.props.violations;
-    var cnt = 0;
-
     return (
       <Form onSubmit={this.handleSubmit}>
         <Table bordered hover responsive>
-          <thead>
-            <tr>
-              <th>#</th>
-              <th>Pravidlo</th>
-              <th>Splněno</th>
-              <th>Akce</th>
-            </tr>
-          </thead>
+          <RulesHeader />
           <tbody>
-            {[...rules.entries()].map(([key, rule]) => (
-              <tr key={key}>
-                <td>{(cnt += 1)}</td>
-                <td>{this.formatRule(rule)}</td>
-                <td>
-                  {violations.get(key) &&
-                    (violations.get(key).satisfied ? (
-                      <span className="text-success">
-                        <i className="fa fa-check" /> ano
-                      </span>
-                    ) : (
-                      <span className="text-danger">
-                        <i className="fa fa-times" /> {violations.get(key).msg}
-                      </span>
-                    ))}
-                </td>
-                <td>
-                  <Button
-                    variant="link text-danger"
-                    onClick={() => {
-                      this.props.deleteRule(rule._id);
-                    }}
-                  >
-                    <i className="fa fa-trash"></i> Smazat
-                  </Button>
-                </td>
-              </tr>
+            {[...this.props.rules.entries()].map(([key, rule], index) => (
+              <Rule
+                key={key}
+                cnt={index + 1}
+                rule={rule}
+                programs={this.props.programs}
+                violation={this.props.violations.get(key)}
+                deleteRule={() => this.props.deleteRule(rule._id)}
+              />
             ))}
-            <tr key="new_rule">
-              <td></td>
-              <td>
-                <Form.Row>
-                  <Col sm="3">
-                    <Form.Control
-                      as="select"
-                      defaultValue="Žádný program"
-                      ref={this.program}
-                    >
-                      <option>Žádný program</option>
-                      {[...this.props.programs.entries()].map(([key, prog]) => (
-                        <option key={key} value={key}>
-                          {this.formatProgram(prog)}
-                        </option>
-                      ))}
-                    </Form.Control>
-                  </Col>
-                  <Col>
-                    <Form.Control
-                      as="select"
-                      ref={this.condition}
-                      onChange={(ev) =>
-                        this.setState({ condition: ev.target.value })
-                      }
-                    >
-                      <option value="is_before_date">
-                        musí proběhnout před
-                      </option>
-                      <option value="is_after_date">musí proběhnout po</option>
-                      <option value="is_before_program">
-                        musí proběhnout před programem
-                      </option>
-                      <option value="is_after_program">
-                        musí proběhnout po programu{" "}
-                      </option>
-                    </Form.Control>
-                  </Col>
-                  {(() => {
-                    switch (this.state.condition) {
-                      case "is_before_date":
-                      case "is_after_date":
-                        return (
-                          <>
-                            <Col sm="2">
-                              <Form.Control
-                                ref={this.time}
-                                defaultValue={formatTime(Date.now())}
-                              />
-                            </Col>
-                            <Col sm="2">
-                              <Form.Control
-                                ref={this.date}
-                                defaultValue={formatDate(Date.now())}
-                              />
-                            </Col>
-                          </>
-                        );
-                      case "is_before_program":
-                      case "is_after_program":
-                        return (
-                          <Col>
-                            <Form.Control
-                              as="select"
-                              defaultValue="Žádný program"
-                              ref={this.program2}
-                            >
-                              <option>Žádný program</option>
-                              {[...this.props.programs.entries()].map(
-                                ([key, prog]) => (
-                                  <option key={key} value={key}>
-                                    {this.formatProgram(prog)}
-                                  </option>
-                                )
-                              )}
-                            </Form.Control>
-                          </Col>
-                        );
-                      default:
-                        return null;
-                    }
-                  })()}
-                </Form.Row>
-              </td>
-              <td></td>
-              <td>
-                <Button variant="success" type="submit">
-                  <i className="fa fa-plus"></i> Přidat
-                </Button>
-              </td>
-            </tr>
+            <NewRule
+              programs={this.props.programs}
+              condition={this.state.condition}
+              setCondition={(condition) =>
+                this.setState({ condition: condition })
+              }
+              firstProgramRef={this.firstProgramRef}
+              secondProgramRef={this.secondProgramRef}
+              conditionRef={this.conditionRef}
+              timeRef={this.timeRef}
+              dateRef={this.dateRef}
+            />
           </tbody>
         </Table>
       </Form>
     );
   }
 
-  formatRule(rule) {
-    const prog_title = this.formatProgram(
-      this.props.programs.get(rule.program)
-    );
-
-    switch (rule.condition) {
-      case "is_before_date":
-        return (
-          <span>
-            <mark>{prog_title}</mark> musí proběhnout před
-            <mark>{formatDateTime(rule.value)}</mark>
-          </span>
-        );
-      case "is_after_date":
-        return (
-          <span>
-            <mark>{prog_title}</mark> musí proběhnout po
-            <mark>{formatDateTime(rule.value)}</mark>
-          </span>
-        );
-      default:
-    }
-
-    var prog2_title = this.formatProgram(this.props.programs.get(rule.value));
-
-    switch (rule.condition) {
-      case "is_before_program":
-        return (
-          <span>
-            <mark>{prog_title}</mark> musí proběhnout před
-            <mark>{prog2_title}</mark>
-          </span>
-        );
-      case "is_after_program":
-        return (
-          <span>
-            <mark>{prog_title}</mark> musí proběhnout po
-            <mark>{prog2_title}</mark>
-          </span>
-        );
-      default:
-        return;
-    }
-  }
-
-  formatProgram(prog) {
-    if (prog) {
-      return `${
-        prog.title === "" ? "(bez názvu)" : prog.title
-      } (${formatDateTime(prog.begin)}) ${
-        prog.groups.length > 0
-          ? ` (skupiny: ${prog.groups.join(", ")})`
-          : " (všechny skupiny)"
-      }`;
-    }
-
-    return "(program nenalezen)";
-  }
-
   handleSubmit(event) {
     event.preventDefault();
 
-    const condition = this.condition.current.value;
+    const condition = this.conditionRef.current.value;
 
     var value;
     switch (condition) {
       case "is_before_date":
       case "is_after_date":
         value =
-          parseDate(this.date.current.value) +
-          parseTime(this.time.current.value);
+          parseDate(this.dateRef.current.value) +
+          parseTime(this.timeRef.current.value);
         break;
       case "is_before_program":
       case "is_after_program":
-        value = this.program2.current.value;
+        value = this.secondProgramRef.current.value;
         break;
       default:
         value = null;
     }
 
     this.props.addRule({
-      program: this.program.current.value,
+      program: this.firstProgramRef.current.value,
       condition: condition,
       value: value,
     });
   }
+}
+
+function RulesHeader() {
+  return (
+    <thead>
+      <tr>
+        <th>#</th>
+        <th>Pravidlo</th>
+        <th>Splněno</th>
+        <th>Akce</th>
+      </tr>
+    </thead>
+  );
+}
+
+function Rule(props) {
+  return (
+    <tr>
+      <td>{props.cnt}</td>
+      <td>{formatRule(props.rule, props.programs)}</td>
+      <td>
+        {props.violation &&
+          (props.violation.satisfied ? (
+            <span className="text-success">
+              <i className="fa fa-check" /> ano
+            </span>
+          ) : (
+            <span className="text-danger">
+              <i className="fa fa-times" /> {props.violation.msg}
+            </span>
+          ))}
+      </td>
+      <td>
+        <Button variant="link text-danger" onClick={props.deleteRule}>
+          <i className="fa fa-trash"></i> Smazat
+        </Button>
+      </td>
+    </tr>
+  );
+}
+
+function NewRule(props) {
+  return (
+    <tr>
+      <td></td>
+      <td>
+        <Form.Row>
+          <Col sm="3">
+            <Form.Control
+              as="select"
+              defaultValue="Žádný program"
+              ref={props.firstProgramRef}
+            >
+              <option>Žádný program</option>
+              {[...props.programs.entries()].map(([key, prog]) => (
+                <option key={key} value={key}>
+                  {formatProgram(prog)}
+                </option>
+              ))}
+            </Form.Control>
+          </Col>
+          <Col>
+            <Form.Control
+              as="select"
+              ref={props.conditionRef}
+              onChange={(ev) => props.setCondition(ev.target.value)}
+            >
+              <option value="is_before_date">musí proběhnout před</option>
+              <option value="is_after_date">musí proběhnout po</option>
+              <option value="is_before_program">
+                musí proběhnout před programem
+              </option>
+              <option value="is_after_program">
+                musí proběhnout po programu{" "}
+              </option>
+            </Form.Control>
+          </Col>
+          {(() => {
+            switch (props.condition) {
+              case "is_before_date":
+              case "is_after_date":
+                return (
+                  <>
+                    <Col sm="2">
+                      <Form.Control
+                        ref={props.timeRef}
+                        defaultValue={formatTime(Date.now())}
+                      />
+                    </Col>
+                    <Col sm="2">
+                      <Form.Control
+                        ref={props.dateRef}
+                        defaultValue={formatDate(Date.now())}
+                      />
+                    </Col>
+                  </>
+                );
+              case "is_before_program":
+              case "is_after_program":
+                return (
+                  <Col>
+                    <Form.Control
+                      as="select"
+                      defaultValue="Žádný program"
+                      ref={props.secondProgramRef}
+                    >
+                      <option>Žádný program</option>
+                      {[...props.programs.entries()].map(([key, prog]) => (
+                        <option key={key} value={key}>
+                          {formatProgram(prog)}
+                        </option>
+                      ))}
+                    </Form.Control>
+                  </Col>
+                );
+              default:
+                return null;
+            }
+          })()}
+        </Form.Row>
+      </td>
+      <td></td>
+      <td>
+        <Button variant="success" type="submit">
+          <i className="fa fa-plus"></i> Přidat
+        </Button>
+      </td>
+    </tr>
+  );
+}
+
+function formatRule(rule, programs) {
+  const prog_title = formatProgram(programs.get(rule.program));
+
+  switch (rule.condition) {
+    case "is_before_date":
+      return (
+        <span>
+          <mark>{prog_title}</mark> musí proběhnout před
+          <mark>{formatDateTime(rule.value)}</mark>
+        </span>
+      );
+    case "is_after_date":
+      return (
+        <span>
+          <mark>{prog_title}</mark> musí proběhnout po
+          <mark>{formatDateTime(rule.value)}</mark>
+        </span>
+      );
+    default:
+  }
+
+  var prog2_title = formatProgram(programs.get(rule.value));
+
+  switch (rule.condition) {
+    case "is_before_program":
+      return (
+        <span>
+          <mark>{prog_title}</mark> musí proběhnout před
+          <mark>{prog2_title}</mark>
+        </span>
+      );
+    case "is_after_program":
+      return (
+        <span>
+          <mark>{prog_title}</mark> musí proběhnout po
+          <mark>{prog2_title}</mark>
+        </span>
+      );
+    default:
+      return;
+  }
+}
+
+function formatProgram(prog) {
+  if (prog) {
+    return `${prog.title === "" ? "(bez názvu)" : prog.title} (${formatDateTime(
+      prog.begin
+    )}) ${
+      prog.groups.length > 0
+        ? ` (skupiny: ${prog.groups.join(", ")})`
+        : " (všechny skupiny)"
+    }`;
+  }
+
+  return "(program nenalezen)";
 }
