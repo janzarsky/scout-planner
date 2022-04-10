47ae27d5ca6a6ac21a23c1fefd40429e1bd32477
Jan Zarsky <jan.zarsky@gmail.com>
2022-04-10 22:51:46 +0200

    Delete groups when deleting all

--- frontend/src/Example.js
+++ frontend/src/Example.js
@@ -5,11 +5,13 @@ export default async function clear(table) {
     Data.getPrograms(table),
     Data.getPackages(table),
     Data.getRules(table),
-  ]).then(([programs, pkgs, rules]) =>
+    Data.getGroups(table),
+  ]).then(([programs, pkgs, rules, groups]) =>
     Promise.all([
       ...[...programs.values()].map((it) => Data.deleteProgram(table, it._id)),
       ...[...pkgs.values()].map((it) => Data.deletePackage(table, it._id)),
       ...[...rules.values()].map((it) => Data.deleteRule(table, it._id)),
+      ...[...groups.values()].map((it) => Data.deleteGroup(table, it._id)),
     ])
   );
 }
