0173d9f7f1679b679ba940e2e37fe92dbcbe5913
Jan Zarsky <jan.zarsky@gmail.com>
2022-07-07 18:21:22 +0200

    backend: add ranges

--- backend/server.js
+++ backend/server.js
@@ -81,7 +81,7 @@ function updateItem(collection) {
   };
 }
 
-["programs", "packages", "rules", "groups"].forEach((collection) => {
+["programs", "packages", "rules", "groups", "ranges"].forEach((collection) => {
   app
     .route(`/api/:table/${collection}`)
     .get(getAllItems(collection))
