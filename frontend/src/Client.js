e97cdedd088e9d8ac0822b667db9f16537c5943c
Jan Zarsky <jan.zarsky@gmail.com>
2022-07-09 08:53:50 +0200

    Rewrite client as a class

--- frontend/src/Client.js
+++ frontend/src/Client.js
@@ -1,65 +1,69 @@
 const config = require("./config.json");
 
-async function get(table, path, id) {
-  const resp = await fetch(`${config.host}/${table}/${path}/${id}`);
-  if (!resp.ok) {
-    throw new Error(`HTTP error: ${resp.status}`);
+export default class Client {
+  constructor(timetable) {
+    ["program", "package", "rule", "group", "range", "user"].forEach(
+      (entity) => {
+        const name = entity.charAt(0).toUpperCase() + entity.slice(1);
+
+        this[`get${name}s`] = () => this.#getAll(`${entity}s`);
+        this[`get${name}`] = (id) => this.#get(`${entity}s`, id);
+        this[`add${name}`] = (data) => this.#post(`${entity}s`, data);
+        this[`update${name}`] = (data) => this.#put(`${entity}s`, data);
+        this[`delete${name}`] = (id) => this.#remove(`${entity}s`, id);
+      }
+    );
+
+    this.basePath = `${config.host}/${timetable}`;
   }
-  return await resp.json();
-}
 
-async function getAll(table, path) {
-  const resp = await fetch(`${config.host}/${table}/${path}`);
-  if (!resp.ok) {
-    throw new Error(`HTTP error: ${resp.status}`);
+  async #get(path, id) {
+    const resp = await fetch(`${this.basePath}/${path}/${id}`);
+    if (!resp.ok) {
+      throw new Error(`HTTP error: ${resp.status}`);
+    }
+    return await resp.json();
   }
-  return await resp.json();
-}
 
-async function post(table, path, data) {
-  const resp = await fetch(`${config.host}/${table}/${path}`, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(data),
-  });
-  if (!resp.ok) {
-    throw new Error(`HTTP error: ${resp.status}`);
+  async #getAll(path) {
+    const resp = await fetch(`${this.basePath}/${path}`);
+    if (!resp.ok) {
+      throw new Error(`HTTP error: ${resp.status}`);
+    }
+    return await resp.json();
   }
-  return await resp.json();
-}
 
-async function put(table, path, data) {
-  const resp = await fetch(`${config.host}/${table}/${path}/${data._id}`, {
-    method: "PUT",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(data),
-  });
-  if (!resp.ok) {
-    throw new Error(`HTTP error: ${resp.status}`);
+  async #post(path, data) {
+    const resp = await fetch(`${this.basePath}/${path}`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data),
+    });
+    if (!resp.ok) {
+      throw new Error(`HTTP error: ${resp.status}`);
+    }
+    return await resp.json();
   }
-  return await resp.json();
-}
 
-async function remove(table, path, id) {
-  const resp = await fetch(`${config.host}/${table}/${path}/${id}`, {
-    method: "DELETE",
-  });
-  if (!resp.ok) {
-    throw new Error(`HTTP error: ${resp.status}`);
+  async #put(path, data) {
+    const resp = await fetch(`${this.basePath}/${path}/${data._id}`, {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data),
+    });
+    if (!resp.ok) {
+      throw new Error(`HTTP error: ${resp.status}`);
+    }
+    return await resp.json();
   }
-  return await resp.json();
+
+  async #remove(path, id) {
+    const resp = await fetch(`${this.basePath}/${path}/${id}`, {
+      method: "DELETE",
+    });
+    if (!resp.ok) {
+      throw new Error(`HTTP error: ${resp.status}`);
+    }
+    return await resp.json();
+  }
 }
-
-var toExport = {};
-
-["program", "package", "rule", "group", "range"].forEach((entity) => {
-  const name = entity.charAt(0).toUpperCase() + entity.slice(1);
-
-  toExport[`get${name}s`] = (table) => getAll(table, `${entity}s`);
-  toExport[`get${name}`] = (table, id) => get(table, `${entity}s`, id);
-  toExport[`add${name}`] = (table, data) => post(table, `${entity}s`, data);
-  toExport[`update${name}`] = (table, data) => put(table, `${entity}s`, data);
-  toExport[`delete${name}`] = (table, id) => remove(table, `${entity}s`, id);
-});
-
-export default toExport;
