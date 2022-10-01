ca2deceddf68522950601c5ba7203a16a3c5b40a
Jan Zarsky <jan.zarsky@gmail.com>
2022-10-01 18:33:06 +0200

    Rewrite TimeIndicator as function

--- frontend/src/components/TimeIndicator.js
+++ frontend/src/components/TimeIndicator.js
@@ -1,17 +1,13 @@
-import React from "react";
-
-export default class TimeIndicator extends React.Component {
-  render() {
-    return (
-      <div
-        className="timeindicator"
-        style={{
-          gridColumnStart: this.props.rect.x + 3,
-          gridRowStart: this.props.rect.y + 2,
-          gridColumnEnd: "span " + this.props.rect.width,
-          gridRowEnd: "span " + this.props.rect.height,
-        }}
-      ></div>
-    );
-  }
+export default function TimeIndicator(props) {
+  return (
+    <div
+      className="timeindicator"
+      style={{
+        gridColumnStart: props.rect.x + 3,
+        gridRowStart: props.rect.y + 2,
+        gridColumnEnd: "span " + props.rect.width,
+        gridRowEnd: "span " + props.rect.height,
+      }}
+    ></div>
+  );
 }
