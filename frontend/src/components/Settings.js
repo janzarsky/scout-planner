e5bcd339e9cbeb7f9895f0a97001d62bc423f58b
Jan Zarsky <jan.zarsky@gmail.com>
2022-10-01 19:10:57 +0200

    Rewrite Settings with hooks

--- frontend/src/components/Settings.js
+++ frontend/src/components/Settings.js
@@ -1,4 +1,4 @@
-import React from "react";
+import { useRef } from "react";
 import Button from "react-bootstrap/Button";
 import Col from "react-bootstrap/Col";
 import Form from "react-bootstrap/Form";
@@ -7,102 +7,87 @@ import { level } from "../helpers/Level";
 import Import from "./Import";
 import Export from "./Export";
 
-export default class Settings extends React.Component {
-  constructor(props) {
-    super(props);
-    this.deleteAll = this.deleteAll.bind(this);
-  }
-
-  render() {
-    return (
-      <>
-        <Container fluid>
-          <Export
-            programs={this.props.programs}
-            pkgs={this.props.pkgs}
-            groups={this.props.groups}
-            rules={this.props.rules}
-            ranges={this.props.ranges}
-            users={this.props.users}
-          />
-          {this.props.userLevel >= level.ADMIN && (
-            <Import
-              programs={this.props.programs}
-              pkgs={this.props.pkgs}
-              groups={this.props.groups}
-              rules={this.props.rules}
-              ranges={this.props.ranges}
-              users={this.props.users}
-              client={this.props.client}
-            />
-          )}
-          {this.props.userLevel >= level.ADMIN && (
-            <Form.Group>
-              <Button onClick={this.deleteAll}>Smazat vše</Button>
-            </Form.Group>
-          )}
-          {this.props.userLevel >= level.EDIT && (
-            <TimeStep
-              timeStep={this.props.timeStep}
-              updateTimeStep={this.props.updateTimeStep}
-            />
-          )}
-        </Container>
-      </>
-    );
-  }
-
-  async deleteAll() {
+export default function Settings(props) {
+  async function deleteAll() {
     await Promise.all([
-      ...this.props.programs.map((it) =>
-        this.props.client.deleteProgram(it._id)
-      ),
-      ...this.props.pkgs.map((it) => this.props.client.deletePackage(it._id)),
-      ...this.props.groups.map((it) => this.props.client.deleteGroup(it._id)),
-      ...this.props.rules.map((it) => this.props.client.deleteRule(it._id)),
-      ...this.props.ranges.map((it) => this.props.client.deleteRange(it._id)),
-      ...this.props.users.map((it) => this.props.client.deleteUser(it._id)),
+      ...props.programs.map((it) => props.client.deleteProgram(it._id)),
+      ...props.pkgs.map((it) => props.client.deletePackage(it._id)),
+      ...props.groups.map((it) => props.client.deleteGroup(it._id)),
+      ...props.rules.map((it) => props.client.deleteRule(it._id)),
+      ...props.ranges.map((it) => props.client.deleteRange(it._id)),
+      ...props.users.map((it) => props.client.deleteUser(it._id)),
     ]).then(() => window.location.reload());
   }
+
+  return (
+    <>
+      <Container fluid>
+        <Export
+          programs={props.programs}
+          pkgs={props.pkgs}
+          groups={props.groups}
+          rules={props.rules}
+          ranges={props.ranges}
+          users={props.users}
+        />
+        {props.userLevel >= level.ADMIN && (
+          <Import
+            programs={props.programs}
+            pkgs={props.pkgs}
+            groups={props.groups}
+            rules={props.rules}
+            ranges={props.ranges}
+            users={props.users}
+            client={props.client}
+          />
+        )}
+        {props.userLevel >= level.ADMIN && (
+          <Form.Group>
+            <Button onClick={deleteAll}>Smazat vše</Button>
+          </Form.Group>
+        )}
+        {props.userLevel >= level.EDIT && (
+          <TimeStep
+            timeStep={props.timeStep}
+            updateTimeStep={props.updateTimeStep}
+          />
+        )}
+      </Container>
+    </>
+  );
 }
 
-class TimeStep extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.timeStepRef = React.createRef();
-  }
+function TimeStep(props) {
+  const timeStepRef = useRef();
 
-  render() {
-    return (
-      <Form onSubmit={this.handleSubmit}>
-        <Form.Row>
-          <Form.Label column sm="2">
-            Základní interval
-          </Form.Label>
-          <Col sm="3">
-            <Form.Control
-              as="select"
-              defaultValue={this.props.timeStep}
-              ref={this.timeStepRef}
-            >
-              <option value={15 * 60 * 1000}>15 min</option>
-              <option value={10 * 60 * 1000}>10 min</option>
-              <option value={5 * 60 * 1000}>5 min</option>
-            </Form.Control>
-          </Col>
-          <Col>
-            <Button type="submit">
-              <i className="fa fa-check"></i> Uložit
-            </Button>
-          </Col>
-        </Form.Row>
-      </Form>
-    );
-  }
-
-  handleSubmit(event) {
+  function handleSubmit(event) {
     event.preventDefault();
-    this.props.updateTimeStep(this.timeStepRef.current.value);
+    props.updateTimeStep(timeStepRef.current.value);
   }
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <Form.Row>
+        <Form.Label column sm="2">
+          Základní interval
+        </Form.Label>
+        <Col sm="3">
+          <Form.Control
+            as="select"
+            defaultValue={props.timeStep}
+            ref={timeStepRef}
+          >
+            <option value={15 * 60 * 1000}>15 min</option>
+            <option value={10 * 60 * 1000}>10 min</option>
+            <option value={5 * 60 * 1000}>5 min</option>
+          </Form.Control>
+        </Col>
+        <Col>
+          <Button type="submit">
+            <i className="fa fa-check"></i> Uložit
+          </Button>
+        </Col>
+      </Form.Row>
+    </Form>
+  );
 }
