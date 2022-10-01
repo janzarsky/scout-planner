2ca0c92ba73f5811268c3d8faa9947c51d1c6114
Jan Zarsky <jan.zarsky@gmail.com>
2022-10-01 19:26:16 +0200

    Move getSettings out of Timetable

--- frontend/src/components/Timetable.js
+++ frontend/src/components/Timetable.js
@@ -22,7 +22,7 @@ export default class Timetable extends React.Component {
   }
 
   render() {
-    const settings = this.getSettings(
+    const settings = getSettings(
       this.props.programs,
       this.props.groups,
       this.props.timeStep
@@ -59,92 +59,6 @@ export default class Timetable extends React.Component {
         <TimeIndicator rect={getTimeIndicatorRect(settings)} />
       </div>
     );
-  }
-
-  getSettings(programs, groups, timeStep) {
-    const hour = parseDuration("1:00");
-
-    if (programs.length === 0)
-      programs = [{ begin: Date.now(), duration: hour }];
-
-    var settings = {};
-
-    settings.days = [];
-    for (const prog of programs) {
-      settings.days.push(getOnlyDate(prog.begin));
-    }
-    settings.days = [...new Set(settings.days)].sort();
-    settings.days = this.addEmptyDays(settings.days);
-
-    settings.dayStart = parseTime("10:00");
-    for (const prog of programs) {
-      const time = getOnlyTime(prog.begin);
-      if (time < settings.dayStart) settings.dayStart = time;
-    }
-
-    settings.dayEnd = parseTime("16:00");
-    for (const prog of programs) {
-      let time = getOnlyTime(prog.begin + prog.duration);
-      if (time === 0) time = parseTime("23:59");
-      if (time > settings.dayEnd) settings.dayEnd = time;
-    }
-
-    settings.timeHeaders = Array.from(
-      { length: Math.ceil((settings.dayEnd - settings.dayStart) / hour) },
-      (_, idx) => settings.dayStart + idx * hour
-    );
-    settings.timeStep = timeStep;
-    settings.timeSpan = Math.ceil(hour / timeStep);
-    settings.groups = [...groups].sort(byOrder);
-    settings.groupCnt = groups.length > 0 ? groups.length : 1;
-
-    return settings;
-  }
-
-  /**
-   * Add empty days so that the timetable is not "confusing"
-   *
-   * The timetable shows only days that contain at least one program. This may be confusing in
-   * certain situations:
-   *  - consider a Fri-Sun event, when you delete everything on Saturday, it will disappear.
-   *    Ideally, the day should remain there.
-   *  - when creating a new event, there should be some free days so that drag and drop is easier,
-   *    so if you start with event on Friday, there should be also Saturday and Sunday
-   *
-   * @param days at least one day must be supplied
-   */
-  addEmptyDays(days) {
-    const dayLength = parseDuration("24:00");
-
-    // if there are at most 3 missing days, add them
-    const missingDaysLimit = 3;
-    const missingDays = (last, curr) =>
-      curr - last < (missingDaysLimit + 2) * dayLength
-        ? Array.from(
-            { length: (curr - last) / dayLength - 1 },
-            (_, i) => last + (i + 1) * dayLength
-          )
-        : [];
-
-    const extendedDays = days.reduce((prev, curr) => {
-      return [
-        ...prev,
-        ...(prev.length > 0 ? missingDays(prev[prev.length - 1], curr) : []),
-        curr,
-      ];
-    }, []);
-
-    // make sure there are always at least 3 days shown
-    const minDaysLimit = 3;
-    if (extendedDays.length < minDaysLimit)
-      return extendedDays.concat(
-        Array.from(
-          { length: minDaysLimit - extendedDays.length },
-          (_, i) => extendedDays[extendedDays.length - 1] + (i + 1) * dayLength
-        )
-      );
-
-    return extendedDays;
   }
 
   *getDroppables(settings) {
@@ -278,6 +192,91 @@ export default class Timetable extends React.Component {
       this.props.updateProgram(prog);
     }
   }
+}
+
+function getSettings(programs, groups, timeStep) {
+  const hour = parseDuration("1:00");
+
+  if (programs.length === 0) programs = [{ begin: Date.now(), duration: hour }];
+
+  var settings = {};
+
+  settings.days = [];
+  for (const prog of programs) {
+    settings.days.push(getOnlyDate(prog.begin));
+  }
+  settings.days = [...new Set(settings.days)].sort();
+  settings.days = addEmptyDays(settings.days);
+
+  settings.dayStart = parseTime("10:00");
+  for (const prog of programs) {
+    const time = getOnlyTime(prog.begin);
+    if (time < settings.dayStart) settings.dayStart = time;
+  }
+
+  settings.dayEnd = parseTime("16:00");
+  for (const prog of programs) {
+    let time = getOnlyTime(prog.begin + prog.duration);
+    if (time === 0) time = parseTime("23:59");
+    if (time > settings.dayEnd) settings.dayEnd = time;
+  }
+
+  settings.timeHeaders = Array.from(
+    { length: Math.ceil((settings.dayEnd - settings.dayStart) / hour) },
+    (_, idx) => settings.dayStart + idx * hour
+  );
+  settings.timeStep = timeStep;
+  settings.timeSpan = Math.ceil(hour / timeStep);
+  settings.groups = [...groups].sort(byOrder);
+  settings.groupCnt = groups.length > 0 ? groups.length : 1;
+
+  return settings;
+}
+
+/**
+ * Add empty days so that the timetable is not "confusing"
+ *
+ * The timetable shows only days that contain at least one program. This may be confusing in
+ * certain situations:
+ *  - consider a Fri-Sun event, when you delete everything on Saturday, it will disappear.
+ *    Ideally, the day should remain there.
+ *  - when creating a new event, there should be some free days so that drag and drop is easier,
+ *    so if you start with event on Friday, there should be also Saturday and Sunday
+ *
+ * @param days at least one day must be supplied
+ */
+function addEmptyDays(days) {
+  const dayLength = parseDuration("24:00");
+
+  // if there are at most 3 missing days, add them
+  const missingDaysLimit = 3;
+  const missingDays = (last, curr) =>
+    curr - last < (missingDaysLimit + 2) * dayLength
+      ? Array.from(
+          { length: (curr - last) / dayLength - 1 },
+          (_, i) => last + (i + 1) * dayLength
+        )
+      : [];
+
+  const extendedDays = days.reduce((prev, curr) => {
+    return [
+      ...prev,
+      ...(prev.length > 0 ? missingDays(prev[prev.length - 1], curr) : []),
+      curr,
+    ];
+  }, []);
+
+  // make sure there are always at least 3 days shown
+  const minDaysLimit = 3;
+  if (extendedDays.length < minDaysLimit)
+    return extendedDays.concat(
+      Array.from(
+        { length: minDaysLimit - extendedDays.length },
+        (_, i) => extendedDays[extendedDays.length - 1] + (i + 1) * dayLength
+      )
+    );
+
+  return extendedDays;
 }
 
 function getTimeIndicatorRect(settings) {
