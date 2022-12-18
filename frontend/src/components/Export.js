5755d7245a698e13992306b9c6512a394a5ba0ca
Jan Zarsky <jan.zarsky@gmail.com>
2022-12-18 17:26:35 +0100

    Make selectors less confusing

--- frontend/src/components/Export.js
+++ frontend/src/components/Export.js
@@ -2,11 +2,11 @@ import Form from "react-bootstrap/Form";
 import { useSelector } from "react-redux";
 
 export default function Export() {
-  const { groups } = useSelector((state) => state.groups);
-  const { ranges } = useSelector((state) => state.ranges);
-  const { packages } = useSelector((state) => state.packages);
-  const { rules } = useSelector((state) => state.rules);
-  const { users } = useSelector((state) => state.users);
+  const groups = useSelector((state) => state.groups.groups);
+  const ranges = useSelector((state) => state.ranges.ranges);
+  const packages = useSelector((state) => state.packages.packages);
+  const rules = useSelector((state) => state.rules.rules);
+  const users = useSelector((state) => state.users.users);
   const { programs, deletedPrograms } = useSelector((state) => state.programs);
 
   const data = JSON.stringify({
