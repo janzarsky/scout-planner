52fc312d5b07fc7b04990d64aacdd4d3d09a359b
Jan Zarsky <jan.zarsky@gmail.com>
2023-02-12 16:41:17 +0100

    Move blockRect checking to getBlocks

--- frontend/src/components/Timetable.js
+++ frontend/src/components/Timetable.js
@@ -82,9 +82,17 @@ function* getBlocks(programs, settings, violations, onEdit) {
   }));
 
   for (const block of blocks) {
-    yield [
-      ...getPrograms(block.programs, block.rect, settings, violations, onEdit),
-    ];
+    if (block.rect.x >= 0 && block.rect.y >= 0) {
+      yield [
+        ...getPrograms(
+          block.programs,
+          block.rect,
+          settings,
+          violations,
+          onEdit
+        ),
+      ];
+    } else console.warn(`The computed rectangle ${block.rect} is invalid`);
   }
 }
 
@@ -97,28 +105,26 @@ function* getPrograms(programs, blockRect, settings, violations, onEdit) {
       settings
     );
 
-    if (blockRect.x >= 0 && blockRect.y >= 0) {
-      const blockOrder = prog.blockOrder ? prog.blockOrder : 0;
-      const relativeRect = {
-        x: programRect.x - blockRect.x,
-        y: programRect.y - blockRect.y + blockOrder,
-        width: programRect.width,
-        height: 1,
-      };
-      const columnCnt = blockRect.width;
+    const blockOrder = prog.blockOrder ? prog.blockOrder : 0;
+    const relativeRect = {
+      x: programRect.x - blockRect.x,
+      y: programRect.y - blockRect.y + blockOrder,
+      width: programRect.width,
+      height: 1,
+    };
+    const columnCnt = blockRect.width;
 
-      yield (
-        <Block key={prog._id} rect={blockRect} columnCnt={columnCnt}>
-          <Program
-            key={prog._id}
-            rect={relativeRect}
-            program={prog}
-            violations={violations.get(prog._id)}
-            onEdit={onEdit}
-          />
-        </Block>
-      );
-    } else console.warn(`The computed rectangle ${blockRect} is invalid`);
+    yield (
+      <Block key={prog._id} rect={blockRect} columnCnt={columnCnt}>
+        <Program
+          key={prog._id}
+          rect={relativeRect}
+          program={prog}
+          violations={violations.get(prog._id)}
+          onEdit={onEdit}
+        />
+      </Block>
+    );
   }
 }
 
