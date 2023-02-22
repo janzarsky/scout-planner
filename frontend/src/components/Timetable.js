367e8863b8933e7128e1b6cb741ab4a43ca2b917
Jan Zarsky <jan.zarsky@gmail.com>
2023-02-22 22:11:18 +0100

    Remove wrapper around programs

--- frontend/src/components/Timetable.js
+++ frontend/src/components/Timetable.js
@@ -101,49 +101,41 @@ function getBlocks(programs, settings, violations, onEdit) {
   });
 }
 
-function groupProgramsToBlocks(programs) {
-  const sortedPrograms = [...programs].sort((a, b) =>
+function groupProgramsToBlocks(unsortedPrograms) {
+  const programs = [...unsortedPrograms].sort((a, b) =>
     a.begin < b.begin ? -1 : 1
   );
-  const programRects = sortedPrograms.map((program) => ({
-    program,
-  }));
   const alreadyInBlock = new Array(programs.length).fill(false);
-
   const blocks = [];
 
   const overlaps = (a, b) =>
     a.filter((i1) => b.find((i2) => i1 === i2)).length > 0;
 
-  for (let i = 0; i < programRects.length; i++) {
+  for (let i = 0; i < programs.length; i++) {
     if (alreadyInBlock[i]) continue;
 
-    const blockPrograms = [programRects[i]];
-    let blockEnd =
-      programRects[i].program.begin + programRects[i].program.duration;
-    let groups = programRects[i].program.groups;
+    const blockPrograms = [programs[i]];
+    let blockEnd = programs[i].begin + programs[i].duration;
+    let groups = programs[i].groups;
 
-    for (let j = i + 1; j < programRects.length; j++) {
-      const progA = programRects[i].program;
-      const progB = programRects[j].program;
-
-      if (progB.begin >= blockEnd) break;
+    for (let j = i + 1; j < programs.length; j++) {
+      if (programs[j].begin >= blockEnd) break;
 
       if (alreadyInBlock[j]) continue;
 
-      if (overlaps(progA.groups, progB.groups)) {
-        blockPrograms.push(programRects[j]);
-        groups = groups.concat(progB.groups);
+      if (overlaps(programs[i].groups, programs[j].groups)) {
+        blockPrograms.push(programs[j]);
+        groups = groups.concat(programs[j].groups);
         alreadyInBlock[j] = true;
-        blockEnd = Math.max(blockEnd, progB.begin + progB.duration);
+        blockEnd = Math.max(blockEnd, programs[j].begin + programs[j].duration);
       }
     }
 
     blocks.push({
-      programs: blockPrograms.map((b) => b.program),
-      begin: blockPrograms[0].program.begin,
-      duration: blockEnd - blockPrograms[0].program.begin,
-      groups,
+      programs: [...blockPrograms],
+      begin: blockPrograms[0].begin,
+      duration: blockEnd - blockPrograms[0].begin,
+      groups: [...groups],
     });
   }
 
