a40301b203daa2f5ffb290dd2fa7022388a68cfd
Jan Zarsky <jan.zarsky@gmail.com>
2023-02-24 17:56:41 +0100

    Move helper function

--- frontend/src/helpers/Sorting.js
+++ frontend/src/helpers/Sorting.js
@@ -7,3 +7,17 @@ export function byOrder(a, b) {
 export function byName(a, b) {
   return a.name.localeCompare(b.name);
 }
+
+export function arraysEqualWithSorting(a, b) {
+  if (a === b) return true;
+  if (a == null || b == null) return false;
+  if (a.length !== b.length) return false;
+
+  const aSorted = [...a].sort();
+  const bSorted = [...b].sort();
+
+  for (var i = 0; i < aSorted.length; ++i) {
+    if (aSorted[i] !== bSorted[i]) return false;
+  }
+  return true;
+}
--- frontend/src/helpers/TimetableUtils.js
+++ frontend/src/helpers/TimetableUtils.js
@@ -1,3 +1,5 @@
+import { arraysEqualWithSorting } from "./Sorting";
+
 export function groupProgramsToBlocks(unsortedPrograms) {
   const programs = [...unsortedPrograms].sort((a, b) =>
     a.begin < b.begin ? -1 : 1
@@ -32,18 +34,4 @@ export function groupProgramsToBlocks(unsortedPrograms) {
   }
 
   return blocks;
-}
-
-function arraysEqualWithSorting(a, b) {
-  if (a === b) return true;
-  if (a == null || b == null) return false;
-  if (a.length !== b.length) return false;
-
-  const aSorted = [...a].sort();
-  const bSorted = [...b].sort();
-
-  for (var i = 0; i < aSorted.length; ++i) {
-    if (aSorted[i] !== bSorted[i]) return false;
-  }
-  return true;
 }
