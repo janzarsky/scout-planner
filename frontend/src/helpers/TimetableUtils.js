3f66dd4eb74e880e53e03b926d1f39e9777e5534
Jan Zarsky <jan.zarsky@gmail.com>
2023-02-24 17:54:59 +0100

    Enforce same subset of groups in a block

--- frontend/src/helpers/TimetableUtils.js
+++ frontend/src/helpers/TimetableUtils.js
@@ -5,24 +5,19 @@ export function groupProgramsToBlocks(unsortedPrograms) {
   const alreadyInBlock = new Array(programs.length).fill(false);
   const blocks = [];
 
-  const overlaps = (a, b) =>
-    a.filter((i1) => b.find((i2) => i1 === i2)).length > 0;
-
   for (let i = 0; i < programs.length; i++) {
     if (alreadyInBlock[i]) continue;
 
     const blockPrograms = [programs[i]];
     let blockEnd = programs[i].begin + programs[i].duration;
-    let groups = programs[i].groups;
 
     for (let j = i + 1; j < programs.length; j++) {
       if (programs[j].begin >= blockEnd) break;
 
       if (alreadyInBlock[j]) continue;
 
-      if (overlaps(programs[i].groups, programs[j].groups)) {
+      if (arraysEqualWithSorting(programs[i].groups, programs[j].groups)) {
         blockPrograms.push(programs[j]);
-        groups = [...new Set([...groups, ...programs[j].groups])];
         alreadyInBlock[j] = true;
         blockEnd = Math.max(blockEnd, programs[j].begin + programs[j].duration);
       }
@@ -32,9 +27,23 @@ export function groupProgramsToBlocks(unsortedPrograms) {
       programs: [...blockPrograms],
       begin: blockPrograms[0].begin,
       duration: blockEnd - blockPrograms[0].begin,
-      groups: [...groups],
+      groups: [...blockPrograms[0].groups],
     });
   }
 
   return blocks;
+}
+
+function arraysEqualWithSorting(a, b) {
+  if (a === b) return true;
+  if (a == null || b == null) return false;
+  if (a.length !== b.length) return false;
+
+  const aSorted = [...a].sort();
+  const bSorted = [...b].sort();
+
+  for (var i = 0; i < aSorted.length; ++i) {
+    if (aSorted[i] !== bSorted[i]) return false;
+  }
+  return true;
 }
--- frontend/src/helpers/TimetableUtils.test.js
+++ frontend/src/helpers/TimetableUtils.test.js
@@ -68,3 +68,48 @@ test("two programs with overlapping time", () => {
     { programs: [progA, progB], begin: 0, duration: 90, groups: ["first"] },
   ]);
 });
+
+test("two programs with overlapping times and groups", () => {
+  const progA = {
+    begin: 0,
+    duration: 60,
+    groups: ["first", "second"],
+  };
+  const progB = {
+    begin: 0,
+    duration: 60,
+    groups: ["second", "third"],
+  };
+
+  expect(groupProgramsToBlocks([progA, progB])).toEqual([
+    { programs: [progA], begin: 0, duration: 60, groups: ["first", "second"] },
+    { programs: [progB], begin: 0, duration: 60, groups: ["second", "third"] },
+  ]);
+});
+
+test("three programs with overlapping time", () => {
+  const progA = {
+    begin: 0,
+    duration: 60,
+    groups: ["first"],
+  };
+  const progB = {
+    begin: 40,
+    duration: 60,
+    groups: ["first"],
+  };
+  const progC = {
+    begin: 80,
+    duration: 60,
+    groups: ["first"],
+  };
+
+  expect(groupProgramsToBlocks([progA, progB, progC])).toEqual([
+    {
+      programs: [progA, progB, progC],
+      begin: 0,
+      duration: 140,
+      groups: ["first"],
+    },
+  ]);
+});
