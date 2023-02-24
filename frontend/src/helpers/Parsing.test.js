a9777bb1ec9f037302ccd1a634528771416206fa
Jan Žárský <jan.zarsky@gmail.com>
2023-02-24 14:26:51 +0100

    Add tests (#118)
    * Add Jest
    
    * Add simple test
    
    * Add running tests to build workflow

---
+++ frontend/src/helpers/Parsing.test.js
@@ -0,0 +1,13 @@
+import { parseIntOrZero } from "./Parsing";
+
+test("valid number", () => {
+  expect(parseIntOrZero("42")).toEqual(42);
+});
+
+test("zero", () => {
+  expect(parseIntOrZero("0")).toEqual(0);
+});
+
+test("invalid number", () => {
+  expect(parseIntOrZero("asdf")).toEqual(0);
+});
