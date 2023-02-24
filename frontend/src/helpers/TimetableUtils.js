5b374ff88d2309c2dc82f81dc72c09a500dd98ac
Jan Zarsky <jan.zarsky@gmail.com>
2023-02-24 14:32:14 +0100

    Move program grouping to separate file

---
+++ frontend/src/helpers/TimetableUtils.js
@@ -0,0 +1,40 @@
+export function groupProgramsToBlocks(unsortedPrograms) {
+  const programs = [...unsortedPrograms].sort((a, b) =>
+    a.begin < b.begin ? -1 : 1
+  );
+  const alreadyInBlock = new Array(programs.length).fill(false);
+  const blocks = [];
+
+  const overlaps = (a, b) =>
+    a.filter((i1) => b.find((i2) => i1 === i2)).length > 0;
+
+  for (let i = 0; i < programs.length; i++) {
+    if (alreadyInBlock[i]) continue;
+
+    const blockPrograms = [programs[i]];
+    let blockEnd = programs[i].begin + programs[i].duration;
+    let groups = programs[i].groups;
+
+    for (let j = i + 1; j < programs.length; j++) {
+      if (programs[j].begin >= blockEnd) break;
+
+      if (alreadyInBlock[j]) continue;
+
+      if (overlaps(programs[i].groups, programs[j].groups)) {
+        blockPrograms.push(programs[j]);
+        groups = groups.concat(programs[j].groups);
+        alreadyInBlock[j] = true;
+        blockEnd = Math.max(blockEnd, programs[j].begin + programs[j].duration);
+      }
+    }
+
+    blocks.push({
+      programs: [...blockPrograms],
+      begin: blockPrograms[0].begin,
+      duration: blockEnd - blockPrograms[0].begin,
+      groups: [...groups],
+    });
+  }
+
+  return blocks;
+}
