24507b71958fa7b77aa2dd35442a4617f28e8aea
Jan Zarsky <jan.zarsky@gmail.com>
2023-03-16 19:02:34 +0100

    Move import logic to separate function

--- frontend/src/components/Import.js
+++ frontend/src/components/Import.js
@@ -14,108 +14,9 @@ export default function Import() {
     event.preventDefault();
 
     const data = JSON.parse(dataToImport);
+    await importData(data, client);
 
-    // data fix
-    if (data.ranges === undefined) data.ranges = [];
-    if (data.users === undefined) data.users = [];
-    if (data.settings === undefined) data.settings = {};
-
-    await Promise.all([
-      // add all packages
-      Promise.all([
-        ...data.pkgs.map((pkg) =>
-          client.addPackage({ ...pkg, _id: undefined }).then(
-            // create package ID replacement map
-            (newPkg) => [pkg._id, newPkg._id]
-          )
-        ),
-      ]).then((pkgs) => new Map(pkgs)),
-      // add all groups
-      Promise.all([
-        ...data.groups.map((group) =>
-          client.addGroup({ ...group, _id: undefined }).then(
-            // create group ID replacement map
-            (newGroup) => [group._id, newGroup._id]
-          )
-        ),
-      ]).then((groups) => new Map(groups)),
-      // add all ranges
-      Promise.all([
-        ...data.ranges.map((range) =>
-          client.addRange({ ...range, _id: undefined }).then(
-            // create range ID replacement map
-            (newRange) => [range._id, newRange._id]
-          )
-        ),
-      ]).then((ranges) => new Map(ranges)),
-      client.updateSettings(data.settings),
-    ])
-      // replace package, group, and range IDs in programs
-      .then(([pkgs, groups, ranges]) =>
-        data.programs.map((prog) => {
-          return {
-            ...prog,
-            pkg: pkgs.get(prog.pkg),
-            groups: prog.groups.map((oldGroup) => groups.get(oldGroup)),
-            ranges: prog.ranges
-              ? Object.fromEntries(
-                  Object.entries(prog.ranges).map(([oldRange, val]) => [
-                    ranges.get(oldRange),
-                    val,
-                  ])
-                )
-              : undefined,
-          };
-        })
-      )
-      // add all programs
-      .then((programs) =>
-        Promise.all(
-          programs.map((prog) =>
-            client
-              .addProgram({
-                ...prog,
-                _id: undefined,
-              })
-              .then(
-                // create program ID replacement map
-                (newProg) => [prog._id, newProg._id]
-              )
-          )
-        )
-      )
-      .then((programs) => new Map(programs))
-      // replace program IDs in rules
-      .then((programs) =>
-        data.rules.map((rule) => {
-          var value = rule.value;
-          if (
-            rule.condition === "is_before_program" ||
-            rule.condition === "is_after_program"
-          )
-            value = programs.get(rule.value);
-          return {
-            ...rule,
-            program: programs.get(rule.program),
-            value: value,
-          };
-        })
-      )
-      // add all rules
-      .then((rules) =>
-        Promise.all(
-          rules.map((rule) => client.addRule({ ...rule, _id: undefined }))
-        )
-      )
-      // add all users (at the end, so there are no issues with permissions)
-      .then(() =>
-        Promise.all([
-          ...data.users.map((user) =>
-            client.addUser({ ...user, _id: undefined })
-          ),
-        ])
-      )
-      .then(() => window.location.reload());
+    window.location.reload();
   }
 
   return (
@@ -133,4 +34,107 @@ export default function Import() {
       </Form.Group>
     </Form>
   );
+}
+
+async function importData(data, client) {
+  // data fixes
+  if (data.ranges === undefined) data.ranges = [];
+  if (data.users === undefined) data.users = [];
+  if (data.settings === undefined) data.settings = {};
+
+  return await Promise.all([
+    // add all packages
+    Promise.all([
+      ...data.pkgs.map((pkg) =>
+        client.addPackage({ ...pkg, _id: undefined }).then(
+          // create package ID replacement map
+          (newPkg) => [pkg._id, newPkg._id]
+        )
+      ),
+    ]).then((pkgs) => new Map(pkgs)),
+    // add all groups
+    Promise.all([
+      ...data.groups.map((group) =>
+        client.addGroup({ ...group, _id: undefined }).then(
+          // create group ID replacement map
+          (newGroup) => [group._id, newGroup._id]
+        )
+      ),
+    ]).then((groups) => new Map(groups)),
+    // add all ranges
+    Promise.all([
+      ...data.ranges.map((range) =>
+        client.addRange({ ...range, _id: undefined }).then(
+          // create range ID replacement map
+          (newRange) => [range._id, newRange._id]
+        )
+      ),
+    ]).then((ranges) => new Map(ranges)),
+    client.updateSettings(data.settings),
+  ])
+    // replace package, group, and range IDs in programs
+    .then(([pkgs, groups, ranges]) =>
+      data.programs.map((prog) => {
+        return {
+          ...prog,
+          pkg: pkgs.get(prog.pkg),
+          groups: prog.groups.map((oldGroup) => groups.get(oldGroup)),
+          ranges: prog.ranges
+            ? Object.fromEntries(
+                Object.entries(prog.ranges).map(([oldRange, val]) => [
+                  ranges.get(oldRange),
+                  val,
+                ])
+              )
+            : undefined,
+        };
+      })
+    )
+    // add all programs
+    .then((programs) =>
+      Promise.all(
+        programs.map((prog) =>
+          client
+            .addProgram({
+              ...prog,
+              _id: undefined,
+            })
+            .then(
+              // create program ID replacement map
+              (newProg) => [prog._id, newProg._id]
+            )
+        )
+      )
+    )
+    .then((programs) => new Map(programs))
+    // replace program IDs in rules
+    .then((programs) =>
+      data.rules.map((rule) => {
+        var value = rule.value;
+        if (
+          rule.condition === "is_before_program" ||
+          rule.condition === "is_after_program"
+        )
+          value = programs.get(rule.value);
+        return {
+          ...rule,
+          program: programs.get(rule.program),
+          value: value,
+        };
+      })
+    )
+    // add all rules
+    .then((rules) =>
+      Promise.all(
+        rules.map((rule) => client.addRule({ ...rule, _id: undefined }))
+      )
+    )
+    // add all users (at the end, so there are no issues with permissions)
+    .then(() =>
+      Promise.all([
+        ...data.users.map((user) =>
+          client.addUser({ ...user, _id: undefined })
+        ),
+      ])
+    );
 }
