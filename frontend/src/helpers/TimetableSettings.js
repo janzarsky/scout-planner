9a084252619df655a47dbe0f9bb2a3e898f18b27
Jan Žárský <jan.zarsky@gmail.com>
2023-03-27 22:28:05 +0200

    Fix day beginning (#170)
    * Move timetable settings to a separate file
    
    * Add basic test for timetable settings
    
    * Fix invalid day starts and ends

---
+++ frontend/src/helpers/TimetableSettings.js
@@ -0,0 +1,61 @@
+import { byOrder } from "../helpers/Sorting";
+import {
+  getOnlyDate,
+  getOnlyTime,
+  parseDuration,
+  parseTime,
+} from "../helpers/DateUtils";
+import { addEmptyDays } from "../helpers/EmptyDays";
+
+export function getTimetableSettings(programs, groups, timeStep, now) {
+  const hour = parseDuration("1:00");
+
+  if (programs.length === 0) programs = [{ begin: now, duration: hour }];
+
+  var settings = {};
+
+  settings.days = [];
+  for (const prog of programs) {
+    settings.days.push(getOnlyDate(prog.begin));
+  }
+  settings.days = [...new Set(settings.days)].sort();
+  settings.days = addEmptyDays(settings.days);
+
+  settings.dayStart = parseTime("10:00");
+  for (const prog of programs) {
+    const time = getOnlyTime(prog.begin);
+    if (time < settings.dayStart) settings.dayStart = time;
+  }
+
+  settings.dayStart = roundDownToWholeHours(settings.dayStart);
+
+  settings.dayEnd = parseTime("16:00");
+  for (const prog of programs) {
+    let time = getOnlyTime(prog.begin + prog.duration);
+    if (time === 0) time = parseTime("23:59");
+    if (time > settings.dayEnd) settings.dayEnd = time;
+  }
+
+  settings.dayEnd = roundUpToWholeHours(settings.dayEnd);
+
+  settings.timeHeaders = Array.from(
+    { length: Math.ceil((settings.dayEnd - settings.dayStart) / hour) },
+    (_, idx) => settings.dayStart + idx * hour
+  );
+  settings.timeStep = timeStep;
+  settings.timeSpan = Math.ceil(hour / timeStep);
+  settings.groups = [...groups].sort(byOrder);
+  settings.groupCnt = groups.length > 0 ? groups.length : 1;
+
+  return settings;
+}
+
+const hour = parseDuration("1:00");
+
+function roundDownToWholeHours(time) {
+  return time - (time % hour);
+}
+
+function roundUpToWholeHours(time) {
+  return time - (time % hour) + (time % hour !== 0 ? hour : 0);
+}
---
+++ frontend/src/helpers/TimetableSettings.test.js
@@ -0,0 +1,121 @@
+import {
+  parseDate,
+  parseDateTime,
+  parseDuration,
+  parseTime,
+} from "./DateUtils";
+import { getTimetableSettings } from "./TimetableSettings";
+
+describe("getTimetableSettings()", () => {
+  it("returns settings for now when there are no programs", () => {
+    expect(
+      getTimetableSettings(
+        [],
+        [],
+        parseDuration("0:15"),
+        parseDateTime("11:30 11.06.2022")
+      )
+    ).toEqual({
+      days: [
+        parseDate("11.06.2022"),
+        parseDate("12.06.2022"),
+        parseDate("13.06.2022"),
+      ],
+      dayStart: parseTime("10:00"),
+      dayEnd: parseTime("16:00"),
+      groupCnt: 1,
+      groups: [],
+      timeSpan: 4,
+      timeStep: parseDuration("0:15"),
+      timeHeaders: [
+        parseTime("10:00"),
+        parseTime("11:00"),
+        parseTime("12:00"),
+        parseTime("13:00"),
+        parseTime("14:00"),
+        parseTime("15:00"),
+      ],
+    });
+  });
+
+  it("returns settings for single program", () => {
+    const prog = {
+      begin: parseDateTime("08:00 10.10.2022"),
+      duration: parseDuration("10:00"),
+    };
+
+    expect(
+      getTimetableSettings(
+        [prog],
+        [],
+        parseDuration("0:15"),
+        parseDateTime("11:30 11.06.2022")
+      )
+    ).toEqual({
+      days: [
+        parseDate("10.10.2022"),
+        parseDate("11.10.2022"),
+        parseDate("12.10.2022"),
+      ],
+      dayStart: parseTime("8:00"),
+      dayEnd: parseTime("18:00"),
+      groupCnt: 1,
+      groups: [],
+      timeSpan: 4,
+      timeStep: parseDuration("0:15"),
+      timeHeaders: [
+        parseTime("08:00"),
+        parseTime("09:00"),
+        parseTime("10:00"),
+        parseTime("11:00"),
+        parseTime("12:00"),
+        parseTime("13:00"),
+        parseTime("14:00"),
+        parseTime("15:00"),
+        parseTime("16:00"),
+        parseTime("17:00"),
+      ],
+    });
+  });
+
+  it("returns rounded day start and end for programs not starting at the top of the hour", () => {
+    const prog = {
+      begin: parseDateTime("08:30 10.10.2022"),
+      duration: parseDuration("10:00"),
+    };
+
+    expect(
+      getTimetableSettings(
+        [prog],
+        [],
+        parseDuration("0:15"),
+        parseDateTime("11:30 11.06.2022")
+      )
+    ).toEqual({
+      days: [
+        parseDate("10.10.2022"),
+        parseDate("11.10.2022"),
+        parseDate("12.10.2022"),
+      ],
+      dayStart: parseTime("8:00"),
+      dayEnd: parseTime("19:00"),
+      groupCnt: 1,
+      groups: [],
+      timeSpan: 4,
+      timeStep: parseDuration("0:15"),
+      timeHeaders: [
+        parseTime("08:00"),
+        parseTime("09:00"),
+        parseTime("10:00"),
+        parseTime("11:00"),
+        parseTime("12:00"),
+        parseTime("13:00"),
+        parseTime("14:00"),
+        parseTime("15:00"),
+        parseTime("16:00"),
+        parseTime("17:00"),
+        parseTime("18:00"),
+      ],
+    });
+  });
+});
