a26dd153b2cba4beec8e343d6da69f051a5e83ca
Jan Žárský <jan.zarsky@gmail.com>
2023-04-02 19:28:23 +0200

    People section (#177)
    * Use client factory for easier stubbing in tests
    
    * Add tab for people
    
    * Add form for new people
    
    * Add data-test attributes
    
    * Allow adding people
    
    * Allow people to be removed
    
    * Allow editing people

---
+++ frontend/cypress/component/People.cy.js
@@ -0,0 +1,97 @@
+/// <reference types="cypress"/>
+
+import { clientFactory } from "../../src/Client";
+import People from "../../src/components/People";
+import { getStore } from "../../src/store";
+import { setTable } from "../../src/store/authSlice";
+import { addPerson } from "../../src/store/peopleSlice";
+
+describe("People", () => {
+  let store;
+
+  beforeEach(() => {
+    store = getStore();
+    store.dispatch(setTable("table1"));
+
+    cy.stub(clientFactory, "getClient")
+      .returns({
+        addPerson: cy
+          .spy(async (person) => ({ _id: "newperson", ...person }))
+          .as("addPerson"),
+        deletePerson: cy.spy(async (id) => {}).as("deletePerson"),
+        updatePerson: cy.spy(async (person) => person).as("updatePerson"),
+      })
+      .log(false);
+  });
+
+  it("empty", () => {
+    cy.mount(<People />, { reduxStore: store });
+    cy.contains("Organizátor");
+    cy.get("[data-test='people-new-name']");
+    cy.get("[data-test='people-new-add']").click();
+  });
+
+  it("list of people", () => {
+    store.dispatch(addPerson({ _id: "person1", name: "Person 1" }));
+    store.dispatch(addPerson({ _id: "person2", name: "Person 2" }));
+
+    cy.mount(<People />, { reduxStore: store });
+
+    cy.contains("Organizátor");
+    cy.contains("Person 1");
+    cy.contains("Person 2");
+  });
+
+  it("add person", () => {
+    cy.mount(<People />, { reduxStore: store });
+    cy.get("[data-test='people-new-name']").clear().type("Person 1");
+    cy.get("[data-test='people-new-add']").click();
+
+    cy.get("@addPerson").should("be.calledOnceWith", { name: "Person 1" });
+
+    cy.wrap(store)
+      .invoke("getState")
+      .its("people.people")
+      .should("deep.include", {
+        _id: "newperson",
+        name: "Person 1",
+      });
+
+    cy.contains("Person 1");
+  });
+
+  it("remove person", () => {
+    store.dispatch(addPerson({ _id: "person1", name: "Person 1" }));
+    cy.mount(<People />, { reduxStore: store });
+
+    cy.contains("Smazat").click();
+
+    cy.get("@deletePerson").should("be.calledOnceWith", "person1");
+
+    cy.wrap(store).invoke("getState").its("people.people").should("be.empty");
+
+    cy.contains("Person 1").should("not.exist");
+  });
+
+  it("edit person", () => {
+    store.dispatch(addPerson({ _id: "person1", name: "Person 1" }));
+    cy.mount(<People />, { reduxStore: store });
+
+    cy.contains("Upravit").click();
+    cy.get("[data-test='people-edit-name']").clear().type("Person 2");
+    cy.get("[data-test='people-edit-save']").click();
+
+    cy.get("@updatePerson").should("be.calledOnceWith");
+
+    cy.wrap(store)
+      .invoke("getState")
+      .its("people.people")
+      .should("deep.include", {
+        _id: "person1",
+        name: "Person 2",
+      });
+
+    cy.contains("Person 2");
+    cy.contains("Upravit");
+  });
+});
---
+++ frontend/src/components/People.js
@@ -0,0 +1,136 @@
+import { useState } from "react";
+import Button from "react-bootstrap/esm/Button";
+import Form from "react-bootstrap/esm/Form";
+import Table from "react-bootstrap/Table";
+import { useDispatch, useSelector } from "react-redux";
+import { clientFactory } from "../Client";
+import { convertLegacyPeople } from "../helpers/PeopleConvertor";
+import { addError } from "../store/errorsSlice";
+import { addPerson, deletePerson, updatePerson } from "../store/peopleSlice";
+
+export default function People() {
+  const { people, legacyPeople } = useSelector((state) => state.people);
+  const allPeople = convertLegacyPeople(legacyPeople, people);
+
+  const [newName, setNewName] = useState("Nový organizátor");
+  const [editedName, setEditedName] = useState();
+  const [editKey, setEditKey] = useState(undefined);
+
+  const dispatch = useDispatch();
+
+  const { table } = useSelector((state) => state.auth);
+  const client = clientFactory.getClient(table);
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    if (editKey) {
+      client
+        .updatePerson({
+          _id: editKey,
+          name: editedName,
+        })
+        .then(
+          (resp) => dispatch(updatePerson(resp)),
+          (e) => dispatch(addError(e.message))
+        );
+      setEditKey(undefined);
+    } else {
+      client
+        .addPerson({
+          name: newName,
+        })
+        .then(
+          (resp) => dispatch(addPerson(resp)),
+          (e) => dispatch(addError(e.message))
+        );
+    }
+  }
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <Table bordered hover responsive>
+        <thead>
+          <tr>
+            <th>Organizátor</th>
+          </tr>
+        </thead>
+        <tbody>
+          {[...allPeople]
+            .sort((a, b) => a.name.localeCompare(b.name))
+            .map((person) =>
+              person._id === editKey ? (
+                <EditedPerson name={editedName} setName={setEditedName} />
+              ) : (
+                <Person
+                  key={person._id}
+                  name={person.name}
+                  editPerson={() => {
+                    setEditKey(person._id);
+                    setEditedName(person.name);
+                  }}
+                  deletePerson={() =>
+                    client.deletePerson(person._id).then(
+                      () => dispatch(deletePerson(person._id)),
+                      (e) => dispatch(addError(e.message))
+                    )
+                  }
+                />
+              )
+            )}
+          <EditedPerson name={newName} setName={setNewName} isNew={true} />
+        </tbody>
+      </Table>
+    </Form>
+  );
+}
+
+function Person({ name, deletePerson, editPerson }) {
+  return (
+    <tr>
+      <td>{name}</td>
+      <td>
+        <span>
+          <Button variant="link" onClick={editPerson}>
+            <i className="fa fa-pencil" /> Upravit
+          </Button>
+          &nbsp;
+          <Button variant="link text-danger" onClick={deletePerson}>
+            <i className="fa fa-trash" /> Smazat
+          </Button>
+        </span>
+      </td>
+    </tr>
+  );
+}
+
+function EditedPerson({ name, setName, isNew = false }) {
+  return (
+    <tr>
+      <td>
+        <Form.Control
+          data-test={isNew ? "people-new-name" : "people-edit-name"}
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </td>
+      <td>
+        <Button
+          data-test={isNew ? "people-new-add" : "people-edit-save"}
+          variant="primary"
+          type="submit"
+        >
+          {isNew ? (
+            <>
+              <i className="fa fa-plus" /> Přidat
+            </>
+          ) : (
+            <>
+              <i className="fa fa-check" /> Uložit
+            </>
+          )}
+        </Button>
+      </td>
+    </tr>
+  );
+}
