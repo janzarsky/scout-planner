d070e23a5e67ebea94ada8000ad14a96407cddf2
Jan Zarsky <jan.zarsky@gmail.com>
2023-04-29 15:24:12 +0200

    Fix tray algorithm

--- frontend/src/components/Tray.test.js
+++ frontend/src/components/Tray.test.js
@@ -39,4 +39,16 @@ describe("getProgramRects", () => {
       [prog2, { x: 0, y: 1, width: 40, height: 1 }],
     ]);
   });
+
+  it("should position third program after overlapping program", () => {
+    const prog1 = { duration: parseDuration("10:00") };
+    const prog2 = { duration: parseDuration("5:00") };
+    const prog3 = { duration: parseDuration("5:00") };
+
+    expect(getProgramRects([prog1, prog2, prog3], settings)).toEqual([
+      [prog1, { x: 4, y: 0, width: 40, height: 1 }],
+      [prog2, { x: 0, y: 1, width: 20, height: 1 }],
+      [prog3, { x: 20, y: 1, width: 20, height: 1 }],
+    ]);
+  });
 });
