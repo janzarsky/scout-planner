1603e2310563a0b2f5226e69cb6171bcf04b11e0
Jan Žárský <jan.zarsky@gmail.com>
2023-05-06 17:53:42 +0200

    Import and export object people (#212)
    * Export people
    
    * Import object people
    
    * Delete all people
    
    * Fix import of string people

--- frontend/src/components/Export.js
+++ frontend/src/components/Export.js
@@ -8,6 +8,7 @@ export default function Export() {
   const rules = useSelector((state) => state.rules.rules);
   const users = useSelector((state) => state.users.users);
   const settings = useSelector((state) => state.settings.settings);
+  const people = useSelector((state) => state.people.people);
   const { programs, deletedPrograms } = useSelector((state) => state.programs);
 
   const data = JSON.stringify({
@@ -18,6 +19,7 @@ export default function Export() {
     ranges,
     users,
     settings,
+    people,
   });
 
   return (
