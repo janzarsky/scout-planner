a9f3dbfb44c02188498cdc03dd570f052831712e
Jan Zarsky <jan.zarsky@gmail.com>
2023-05-07 16:11:31 +0200

    Add program migration functions

--- frontend/src/helpers/PeopleMigration.js
+++ frontend/src/helpers/PeopleMigration.js
@@ -1,5 +1,6 @@
 import { addError } from "../store/errorsSlice";
 import { addPerson, setPeopleMigrationState } from "../store/peopleSlice";
+import { updateProgram } from "../store/programsSlice";
 
 export async function migratePeople(programs, people, client, dispatch) {
   const legacyPeople = getLegacyPeople(programs);
@@ -46,14 +47,58 @@ function getPeopleToBeAdded(legacyPeople, existingPeople) {
 }
 
 export async function migratePrograms(programs, people, client, dispatch) {
-  // TODO
+  const programsToBeUpdated = getProgramsToBeUpdated(programs, people);
+
   dispatch(setPeopleMigrationState("pendingPrograms"));
-  console.log("Migrating programs");
-  dispatch(setPeopleMigrationState("finishedPrograms"));
+
+  try {
+    await updatePrograms(programsToBeUpdated, client, dispatch);
+    dispatch(setPeopleMigrationState("finishedPrograms"));
+  } catch (e) {
+    dispatch(addError(e.message));
+  }
+}
+
+function getProgramsToBeUpdated(programs, people) {
+  return programs.flatMap((prog) => {
+    const stringPeople = prog.people.filter(
+      (person) => typeof person === "string"
+    );
+
+    if (stringPeople.length > 0) {
+      const objectPeople = prog.people.filter(
+        (person) => typeof person !== "string"
+      );
+      const convertedPeople = stringPeople
+        .map((name) => people.find((person) => person.name === name))
+        .filter((person) => person !== undefined)
+        .filter((person) => !objectPeople.find((p) => p._id === person._id))
+        .map((person) => ({ person: person._id }));
+      return [{ ...prog, people: [...convertedPeople, ...objectPeople] }];
+    }
+
+    return [];
+  });
+}
+
+async function updatePrograms(programs, client, dispatch) {
+  return await Promise.all(
+    programs.map((program) =>
+      client.updateProgram(program).then(
+        (resp) => {
+          dispatch(updateProgram(resp));
+          return resp._id;
+        },
+        (e) => dispatch(addError(e.message))
+      )
+    )
+  );
 }
 
 export const testing = {
   addMissingPeople,
   getLegacyPeople,
   getPeopleToBeAdded,
+  getProgramsToBeUpdated,
+  updatePrograms,
 };
