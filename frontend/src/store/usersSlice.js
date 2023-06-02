e278e0efc5abebf0d66410744418f2abd7145b32
Jan Zarsky <jan.zarsky@gmail.com>
2023-06-02 18:30:26 +0200

    Load public level in users slice

--- frontend/src/store/usersSlice.js
+++ frontend/src/store/usersSlice.js
@@ -1,13 +1,25 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { level } from "../helpers/Level";
 
-export const getUsers = createAsyncThunk(
-  "users/getUsers",
-  async (client) => await client.getUsers()
-);
+export const getUsers = createAsyncThunk("users/getUsers", async (client) => {
+  // checking whether the function is present (effectively signals whether firestore flag is on)
+  if (client.getPublicLevel) {
+    return await Promise.all([client.getUsers(), client.getPublicLevel()]).then(
+      ([users, publicLevel]) => ({ users, publicLevel })
+    );
+  } else {
+    return { users: await client.getUsers() };
+  }
+});
 
 export const usersSlice = createSlice({
   name: "users",
-  initialState: { users: [], loading: "idle", error: null },
+  initialState: {
+    users: [],
+    publicLevel: level.NONE,
+    loading: "idle",
+    error: null,
+  },
   reducers: {
     addUser(state, action) {
       state.users.push(action.payload);
@@ -31,7 +43,9 @@ export const usersSlice = createSlice({
 
     builder.addCase(getUsers.fulfilled, (state, action) => {
       if (state.loading === "pending") {
-        state.users = action.payload;
+        state.users = action.payload.users;
+        if (action.payload.publicLevel)
+          state.publicLevel = action.payload.publicLevel;
         state.loading = "idle";
       }
     });
