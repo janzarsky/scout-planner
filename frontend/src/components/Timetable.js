ccfe59321d1b4fafeba0938bb0b376d7821f6d59
Jan Zarsky <jan.zarsky@gmail.com>
2023-06-11 21:38:26 +0200

    Move droppables to separate file

--- frontend/src/components/Timetable.js
+++ frontend/src/components/Timetable.js
@@ -13,6 +13,7 @@ import { getTimetableSettings } from "../helpers/TimetableSettings";
 import { getProgramRects, sortTrayPrograms } from "./Tray";
 import { useCallback, useMemo, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { Droppable, Droppables } from "./Droppables";
 
 export default function Timetable({ violations, timeProvider = null }) {
   const dispatch = useDispatch();
@@ -198,21 +199,6 @@ function getProgramData(prog, blockRect, settings, violations) {
   };
 }
 
-function Droppables({ settings, onDrop }) {
-  const data = useMemo(() => getDroppablesData(settings), [settings]);
-
-  return data.map(({ key, x, y, begin, group }) => (
-    <Droppable
-      key={key}
-      x={x}
-      y={y}
-      begin={begin}
-      group={group}
-      onDrop={onDrop}
-    />
-  ));
-}
-
 function getBlockDroppablesData(width, height, blockBegin, timeStep, groupId) {
   return [...Array(width).keys()].flatMap((x) =>
     [...Array(height).keys()].map((y) => ({
@@ -222,27 +208,6 @@ function getBlockDroppablesData(width, height, blockBegin, timeStep, groupId) {
       begin: blockBegin + x * timeStep,
       group: groupId,
     }))
-  );
-}
-
-function getDroppablesData(settings) {
-  // ensure there is always at least one group
-  const groups = settings.groups.length > 0 ? settings.groups : [{ _id: null }];
-
-  return settings.days.flatMap((date, idxDate) =>
-    settings.timeHeaders.flatMap((time, idxTime) =>
-      [...Array(settings.timeSpan).keys()].flatMap((idxSpan) => {
-        const begin = date + time + idxSpan * settings.timeStep;
-
-        return groups.map((group, idxGroup) => ({
-          key: `${begin}-${group._id}`,
-          x: 3 + idxTime * settings.timeSpan + idxSpan,
-          y: 2 + idxDate * settings.groupCnt + idxGroup,
-          begin,
-          group: group._id,
-        }));
-      })
-    )
   );
 }
 
@@ -277,31 +242,6 @@ function getGroupHeaders(settings) {
         name={group.name}
       />
     ))
-  );
-}
-
-function Droppable({ onDrop, x, y, begin, group }) {
-  const { programs } = useSelector((state) => state.programs);
-  const navigate = useNavigate();
-
-  const [{ isOver }, drop] = useDrop(
-    () => ({
-      accept: "program",
-      drop: (item) => onDrop(item, begin, group, programs),
-      collect: (monitor) => ({
-        isOver: !!monitor.isOver(),
-      }),
-    }),
-    [programs]
-  );
-
-  return (
-    <div
-      ref={drop}
-      className={"droppable " + (isOver ? "drag-over" : "")}
-      style={{ gridColumnStart: x, gridRowStart: y }}
-      onClick={() => navigate("add", { state: { begin, groupId: group } })}
-    />
   );
 }
 
