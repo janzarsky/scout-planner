6c348c490365c43aa27523050979ef2e8425b455
Jan Žárský <jan.zarsky@gmail.com>
2023-06-11 21:58:10 +0200

    Create headers components (#310)
    * Create headers components
    
    * Move headers into separate file

---
+++ frontend/src/components/Headers.js
@@ -0,0 +1,75 @@
+import { formatDay } from "../helpers/DateUtils";
+
+export function TimeHeaders({ settings }) {
+  return settings.timeHeaders.map((time, idx) => (
+    <TimeHeader
+      key={time}
+      time={new Date(time)}
+      pos={idx * settings.timeSpan + 3}
+      span={settings.timeSpan}
+    />
+  ));
+}
+
+function TimeHeader({ time, pos, span }) {
+  return (
+    <div
+      className="timeheader"
+      style={{
+        gridColumnStart: pos,
+        gridColumnEnd: "span " + span,
+      }}
+    >
+      {time.getUTCHours()}
+      <span className="timeheader-minutes">:00</span>
+    </div>
+  );
+}
+
+export function DateHeaders({ settings }) {
+  return settings.days.map((date, idx) => (
+    <DateHeader
+      key={date}
+      date={new Date(date)}
+      pos={idx * settings.groupCnt + 2}
+      span={settings.groupCnt}
+    />
+  ));
+}
+
+function DateHeader({ date, pos, span }) {
+  return (
+    <div
+      className="dateheader"
+      style={{
+        gridRowStart: pos,
+        gridRowEnd: "span " + span,
+      }}
+    >
+      {formatDay(date.getTime())}
+      <br />
+      {date.getUTCDate()}.<br />
+      {date.getUTCMonth() + 1}.
+    </div>
+  );
+}
+
+export function GroupHeaders({ settings }) {
+  return settings.days.flatMap((date, idx) =>
+    settings.groups.map((group, groupIdx) => (
+      <GroupHeader
+        key={`group,${date},${group._id}`}
+        pos={idx * settings.groupCnt + groupIdx + 2}
+        name={group.name}
+      />
+    ))
+  );
+}
+
+function GroupHeader({ pos, name }) {
+  return (
+    <div className="groupheader" style={{ gridRowStart: pos }}>
+      {name}
+    </div>
+  );
+}
--- frontend/src/components/Timetable.js
+++ frontend/src/components/Timetable.js
@@ -2,7 +2,6 @@ import { DndProvider, useDrop } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
 import { useDispatch, useSelector } from "react-redux";
 import { clientFactory } from "../Client";
-import { formatDay } from "../helpers/DateUtils";
 import { getRect, groupProgramsToBlocks } from "../helpers/TimetableUtils";
 import { level } from "../helpers/Level";
 import { addError } from "../store/errorsSlice";
@@ -18,6 +17,7 @@ import {
   Droppables,
   getBlockDroppablesData,
 } from "./Droppables";
+import { DateHeaders, GroupHeaders, TimeHeaders } from "./Headers";
 
 export default function Timetable({ violations, timeProvider = null }) {
   const dispatch = useDispatch();
@@ -92,9 +92,9 @@ export default function Timetable({ violations, timeProvider = null }) {
         {userLevel >= level.EDIT && (
           <Droppables settings={settings} onDrop={onDroppableDrop} />
         )}
-        {getTimeHeaders(settings)}
-        {getDateHeaders(settings)}
-        {getGroupHeaders(settings)}
+        <TimeHeaders settings={settings} />
+        <DateHeaders settings={settings} />
+        <GroupHeaders settings={settings} />
         {getBlocks(blocksData, onDroppableDrop)}
         {timeIndicatorRect && (
           <TimeIndicator
@@ -192,80 +192,6 @@ function getProgramData(prog, blockRect, settings, violations) {
     program: prog,
     violations: violations.get(prog._id),
   };
-}
-
-function getTimeHeaders(settings) {
-  return settings.timeHeaders.map((time, idx) => (
-    <TimeHeader
-      key={time}
-      time={new Date(time)}
-      pos={idx * settings.timeSpan + 3}
-      span={settings.timeSpan}
-    />
-  ));
-}
-
-function getDateHeaders(settings) {
-  return settings.days.map((date, idx) => (
-    <DateHeader
-      key={date}
-      date={new Date(date)}
-      pos={idx * settings.groupCnt + 2}
-      span={settings.groupCnt}
-    />
-  ));
-}
-
-function getGroupHeaders(settings) {
-  return settings.days.flatMap((date, idx) =>
-    settings.groups.map((group, groupIdx) => (
-      <GroupHeader
-        key={`group,${date},${group._id}`}
-        pos={idx * settings.groupCnt + groupIdx + 2}
-        name={group.name}
-      />
-    ))
-  );
-}
-
-function TimeHeader({ time, pos, span }) {
-  return (
-    <div
-      className="timeheader"
-      style={{
-        gridColumnStart: pos,
-        gridColumnEnd: "span " + span,
-      }}
-    >
-      {time.getUTCHours()}
-      <span className="timeheader-minutes">:00</span>
-    </div>
-  );
-}
-
-function DateHeader({ date, pos, span }) {
-  return (
-    <div
-      className="dateheader"
-      style={{
-        gridRowStart: pos,
-        gridRowEnd: "span " + span,
-      }}
-    >
-      {formatDay(date.getTime())}
-      <br />
-      {date.getUTCDate()}.<br />
-      {date.getUTCMonth() + 1}.
-    </div>
-  );
-}
-
-function GroupHeader({ pos, name }) {
-  return (
-    <div className="groupheader" style={{ gridRowStart: pos }}>
-      {name}
-    </div>
-  );
 }
 
 function Block({ rect, children }) {
