60e054fdc34ff191aee1c176542e22a03ea29dc9
Jan Zarsky <jan.zarsky@gmail.com>
2023-07-09 13:01:35 +0200

    Remove Users tests with old client

--- cypress/component/Users.cy.js
+++ cypress/component/Users.cy.js
@@ -1,6 +1,5 @@
 /// <reference types="cypress"/>
 
-import { clientFactory } from "../../src/Client";
 import { firestoreClientFactory } from "../../src/FirestoreClient";
 import Users from "../../src/components/Users";
 import { level } from "../../src/helpers/Level";
@@ -11,18 +10,6 @@ import { addUser, setPublicLevel } from "../../src/store/usersSlice";
 describe("Users", () => {
   let store;
 
-  const config = require("../../src/config.json");
-  var localConfig = {};
-
-  try {
-    localConfig = require("../../src/config.local.json");
-  } catch {}
-
-  const completeConfig = {
-    ...config,
-    ...localConfig,
-  };
-
   beforeEach(() => {
     const client = {
       addUser: cy
@@ -31,17 +18,13 @@ describe("Users", () => {
       updateUser: cy.spy(async (user) => user).as("updateUser"),
       setPublicLevel: cy.spy(async (level) => level).as("setPublicLevel"),
     };
-    if (completeConfig.firestore)
-      cy.stub(firestoreClientFactory, "getClient").returns(client).log(false);
-    else cy.stub(clientFactory, "getClient").returns(client).log(false);
+    cy.stub(firestoreClientFactory, "getClient").returns(client).log(false);
 
     store = getStore();
   });
 
   function setPublicLevelOrUser(level) {
-    if (completeConfig.firestore) store.dispatch(setPublicLevel(level));
-    else
-      store.dispatch(addUser({ _id: "public", email: "public", level: level }));
+    store.dispatch(setPublicLevel(level));
   }
 
   describe("public user", () => {
@@ -55,16 +38,6 @@ describe("Users", () => {
       cy.contains("Pro změnu veřejného přístupu se přihlaste");
     });
 
-    !completeConfig.firestore &&
-      it("displays warning when not logged in and there is an implicit public access", () => {
-        store.dispatch(testing.setUserLevel(level.ADMIN));
-        cy.mount(<Users />, { reduxStore: store });
-
-        cy.contains("Kdokoliv");
-        cy.contains("spravovat uživatele");
-        cy.contains("Pro změnu veřejného přístupu se přihlaste");
-      });
-
     it("displays warning when not logged in and there is an explicit public access", () => {
       store.dispatch(testing.setUserLevel(level.ADMIN));
       setPublicLevelOrUser(level.ADMIN);
@@ -74,17 +47,6 @@ describe("Users", () => {
       cy.contains("spravovat uživatele");
       cy.contains("Pro změnu veřejného přístupu se přihlaste");
     });
-
-    !completeConfig.firestore &&
-      it("displays instructions when logged in and there is an implicit public access", () => {
-        store.dispatch(testing.setUserLevel(level.ADMIN));
-        cy.mount(<Users userEmail="test@email.com" />, { reduxStore: store });
-
-        cy.contains("Kdokoliv");
-        cy.contains("spravovat uživatele");
-        cy.contains("Pro změnu veřejného přístupu nejdříve nastavte oprávnění");
-        cy.contains("test@email.com");
-      });
 
     it("displays instructions when logged in and there is an explicit public access", () => {
       store.dispatch(testing.setUserLevel(level.ADMIN));
@@ -126,56 +88,11 @@ describe("Users", () => {
       cy.get("select").first().select("zobrazovat");
       cy.contains("Uložit").click();
 
-      if (completeConfig.firestore)
-        cy.get("@setPublicLevel").should(
-          "have.been.calledOnceWith",
-          level.VIEW
-        );
-      else
-        cy.get("@updateUser").should("have.been.calledOnceWith", {
-          _id: "public",
-          email: "public",
-          level: level.VIEW,
-        });
+      cy.get("@setPublicLevel").should("have.been.calledOnceWith", level.VIEW);
     });
-
-    !completeConfig.firestore &&
-      it("adds public user when there is not one yet", () => {
-        store.dispatch(testing.setUserLevel(level.ADMIN));
-        store.dispatch(
-          addUser({ _id: "user1", email: "test@email.com", level: level.ADMIN })
-        );
-        cy.mount(<Users userEmail="test@email.com" />, { reduxStore: store });
-
-        cy.contains("Upravit").first().click();
-        cy.get("select").first().select("zobrazovat");
-        cy.contains("Uložit").click();
-
-        cy.get("@addUser").should("have.been.calledOnceWith", {
-          email: "public",
-          level: level.VIEW,
-        });
-      });
   });
 
   describe("current user", () => {
-    !completeConfig.firestore &&
-      it("allows editing when there is an implicit public access", () => {
-        store.dispatch(testing.setUserLevel(level.ADMIN));
-        store.dispatch(
-          addUser({ _id: "user1", email: "test@email.com", level: level.NONE })
-        );
-        cy.mount(<Users userEmail="test@email.com" />, { reduxStore: store });
-
-        cy.get("tbody tr")
-          .eq(1)
-          .within(() => {
-            cy.contains("test@email.com");
-            cy.contains("žádné");
-            cy.contains("Upravit");
-          });
-      });
-
     it("allows editing when there is an explicit public access", () => {
       store.dispatch(testing.setUserLevel(level.ADMIN));
       setPublicLevelOrUser(level.ADMIN);
@@ -272,17 +189,11 @@ describe("Users", () => {
     cy.get("select").select("zobrazovat");
     cy.contains("Přidat").click();
 
-    if (completeConfig.firestore)
-      cy.get("@updateUser").should("have.been.calledOnceWith", {
-        _id: "another@email.com",
-        email: "another@email.com",
-        level: level.VIEW,
-      });
-    else
-      cy.get("@addUser").should("have.been.calledOnceWith", {
-        email: "another@email.com",
-        level: level.VIEW,
-      });
+    cy.get("@updateUser").should("have.been.calledOnceWith", {
+      _id: "another@email.com",
+      email: "another@email.com",
+      level: level.VIEW,
+    });
 
     cy.get("tbody tr")
       .eq(1)
