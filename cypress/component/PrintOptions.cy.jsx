30ee6863b4be804965310c6ff494a228c531fc6c
Jan Žárský <jan.zarsky@gmail.com>
2023-07-31 21:02:33 +0200

    Print section (#377)
    * Add print options component
    
    * Add A4 print button
    
    * Use multiple options

---
+++ cypress/component/PrintOptions.cy.jsx
@@ -0,0 +1,16 @@
+/// <reference types="cypress"/>
+
+import PrintOptions from "../../src/components/PrintOptions";
+
+describe("Print options", () => {
+  it("prints A4 version", () => {
+    const callback = cy.stub().as("callback");
+
+    cy.mount(<PrintOptions printCallback={callback} />);
+
+    cy.contains("Tisk");
+    cy.contains("A4").click();
+
+    cy.get("@callback").should("have.been.calledOnceWith", "a4");
+  });
+});
