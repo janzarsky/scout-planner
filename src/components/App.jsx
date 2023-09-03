b6b5bf9c50badcbd9b2b49e2cc28499735486633
Jan Zarsky <jan.zarsky@gmail.com>
2023-09-03 14:43:13 +0200

    Move NavBar to a separate file

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,9 +7,6 @@ import Groups from "./Groups";
 import People from "./People";
 import Ranges from "./Ranges";
 import Rules from "./Rules";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
 import Alert from "react-bootstrap/Alert";
 import { firestoreClientFactory } from "../FirestoreClient";
 import { checkRules } from "../Checker";
@@ -25,17 +22,17 @@ import { getRules } from "../store/rulesSlice";
 import { getUsers } from "../store/usersSlice";
 import { getPrograms } from "../store/programsSlice";
 import { getPermissions } from "../store/authSlice";
-import { PackageFilter, PackageFilterToggle } from "./PackageFilter";
-import { ViewSettings, ViewSettingsToggle } from "./ViewSettings";
-import { RangesSettings, RangesSettingsToggle } from "./RangesSettings";
+import { PackageFilter } from "./PackageFilter";
+import { ViewSettings } from "./ViewSettings";
+import { RangesSettings } from "./RangesSettings";
 import { getSettings } from "../store/settingsSlice";
 import { getPeople } from "../store/peopleSlice";
 import { useAuth } from "./AuthProvider";
-import { NavLink, Route, Routes } from "react-router-dom";
-import { PeopleFilter, PeopleFilterToggle } from "./PeopleFilter";
+import { Route, Routes } from "react-router-dom";
+import { PeopleFilter } from "./PeopleFilter";
 import { PrintWrapper } from "./PrintOptions";
-import { GoogleLogin } from "./GoogleLogin";
 import { Notifications } from "./Notifications";
+import { NavBar } from "./NavBar";
 
 export default function App() {
   const [violations, setViolations] = useState(new Map());
@@ -241,97 +238,6 @@ export default function App() {
         )}
       </Routes>
     </div>
-  );
-}
-
-export function NavBar({ rulesSatisfied }) {
-  const userLevel = useSelector((state) => state.auth.userLevel);
-  const { peopleFilter, printing } = useSelector((state) => state.config);
-
-  return (
-    <Navbar bg="light" className="control-panel" expand="lg">
-      <Container fluid className="ps-0 pe-0">
-        <Nav.Link as={NavLink} to="" end>
-          Harmonogram
-        </Nav.Link>
-        <Navbar.Toggle
-          aria-controls="navbar-toggle"
-          data-test="navbar-toggle"
-        />
-        <Navbar.Collapse id="navbar-toggle">
-          {userLevel >= level.VIEW && (
-            <Nav.Link as={NavLink} to="rules">
-              Pravidla{" "}
-              {rulesSatisfied ? (
-                <i className="fa fa-check text-success" />
-              ) : (
-                <i className="fa fa-times text-danger" />
-              )}
-            </Nav.Link>
-          )}
-          {userLevel >= level.VIEW && (
-            <Nav.Link as={NavLink} to="stats" end>
-              Statistiky
-            </Nav.Link>
-          )}
-          {userLevel >= level.VIEW && (
-            <NavDropdown title="Nastavení">
-              {userLevel >= level.EDIT && (
-                <NavDropdown.Item as={NavLink} to="packages" end>
-                  Balíčky
-                </NavDropdown.Item>
-              )}
-              {userLevel >= level.EDIT && (
-                <NavDropdown.Item as={NavLink} to="groups" end>
-                  Skupiny
-                </NavDropdown.Item>
-              )}
-              {userLevel >= level.EDIT && (
-                <NavDropdown.Item as={NavLink} to="people" end>
-                  Organizátoři
-                </NavDropdown.Item>
-              )}
-              {userLevel >= level.EDIT && (
-                <NavDropdown.Item as={NavLink} to="ranges" end>
-                  Linky
-                </NavDropdown.Item>
-              )}
-              {userLevel >= level.ADMIN && (
-                <NavDropdown.Item as={NavLink} to="users" end>
-                  Uživatelé
-                </NavDropdown.Item>
-              )}
-              {userLevel >= level.VIEW && (
-                <NavDropdown.Item as={NavLink} to="settings" end>
-                  Nastavení
-                </NavDropdown.Item>
-              )}
-              {printing && userLevel >= level.VIEW && (
-                <NavDropdown.Item as={NavLink} to="print" end>
-                  Tisk
-                </NavDropdown.Item>
-              )}
-            </NavDropdown>
-          )}
-          <Routes>
-            <Route
-              index
-              element={
-                <>
-                  {userLevel >= level.VIEW && <PackageFilterToggle />}
-                  {peopleFilter && userLevel >= level.VIEW && (
-                    <PeopleFilterToggle />
-                  )}
-                  {userLevel >= level.VIEW && <ViewSettingsToggle />}
-                  {userLevel >= level.VIEW && <RangesSettingsToggle />}
-                </>
-              }
-            />
-          </Routes>
-          <GoogleLogin />
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
   );
 }
 
