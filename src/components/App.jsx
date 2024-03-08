e52b2d69e343adc3f8ec0dd0d760c1f911de3520
Jan Zarsky <jan.zarsky@gmail.com>
2024-03-08 17:55:53 +0100

    Add about page route

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,6 +7,7 @@ import Groups from "./Groups";
 import People from "./People";
 import Ranges from "./Ranges";
 import Rules from "./Rules";
+import About from "./About";
 import Alert from "react-bootstrap/Alert";
 import { firestoreClientFactory } from "../FirestoreClient";
 import { checkRules } from "../Checker";
@@ -231,6 +232,7 @@ export default function App() {
             />
           }
         />
+        <Route path="about" element={<About />} />
       </Routes>
     </div>
   );
