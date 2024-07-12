138fd28e67d402d7f9a95cae9415a0e0c00cdabb
Jan Zarsky <jan.zarsky@gmail.com>
2024-07-12 18:31:07 +0200

    Create packages API

--- src/store/index.js
+++ src/store/index.js
@@ -13,6 +13,7 @@ import errorsReducer from "./errorsSlice";
 import configReducer from "./configSlice";
 import { rangesApi } from "./rangesApi";
 import { groupsApi } from "./groupsApi";
+import { packagesApi } from "./packagesApi";
 
 export function getStore() {
   return configureStore({
@@ -31,10 +32,12 @@ export function getStore() {
       config: configReducer,
       [rangesApi.reducerPath]: rangesApi.reducer,
       [groupsApi.reducerPath]: groupsApi.reducer,
+      [packagesApi.reducerPath]: packagesApi.reducer,
     },
     middleware: (getDefaultMiddleware) =>
       getDefaultMiddleware()
         .concat(rangesApi.middleware)
-        .concat(groupsApi.middleware),
+        .concat(groupsApi.middleware)
+        .concat(packagesApi.middleware),
   });
 }
---
+++ src/store/packagesApi.js
@@ -0,0 +1,49 @@
+import { createApi, fakeBaseQuery } from "@reduxjs/toolkit/query/react";
+import { firestoreClientFactory } from "../FirestoreClient";
+
+export const packagesApi = createApi({
+  baseQuery: fakeBaseQuery(),
+  reducerPath: "packagesApi",
+  tagTypes: ["packages"],
+  endpoints: (builder) => ({
+    getPackages: builder.query({
+      async queryFn(table) {
+        const client = firestoreClientFactory.getClient(table);
+        const data = await client.getPackages();
+        return { data };
+      },
+      providesTags: ["packages"],
+    }),
+    addPackage: builder.mutation({
+      async queryFn({ table, data }) {
+        const client = firestoreClientFactory.getClient(table);
+        await client.addPackage(data);
+        return { data: null };
+      },
+      invalidatesTags: ["packages"],
+    }),
+    updatePackage: builder.mutation({
+      async queryFn({ table, data }) {
+        const client = firestoreClientFactory.getClient(table);
+        await client.updatePackage(data);
+        return { data: null };
+      },
+      invalidatesTags: ["packages"],
+    }),
+    deletePackage: builder.mutation({
+      async queryFn({ table, id }) {
+        const client = firestoreClientFactory.getClient(table);
+        await client.deletePackage(id);
+        return { data: null };
+      },
+      invalidatesTags: ["packages"],
+    }),
+  }),
+});
+
+export const {
+  useGetPackagesQuery,
+  useAddPackageMutation,
+  useUpdatePackageMutation,
+  useDeletePackageMutation,
+} = packagesApi;
