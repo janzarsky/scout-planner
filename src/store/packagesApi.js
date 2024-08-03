4a4dccd641474507c0b7b16842214c21db6114fb
Jan Zarsky <jan.zarsky@gmail.com>
2024-08-03 12:56:46 +0200

    Add streaming updates for packages

--- src/store/packagesApi.js
+++ src/store/packagesApi.js
@@ -1,5 +1,9 @@
 import { createApi, fakeBaseQuery } from "@reduxjs/toolkit/query/react";
 import { firestoreClientFactory } from "../FirestoreClient";
+import config from "../config.json";
+import localConfig from "../config.local.json";
+
+const streamingUpdates = { ...config, ...localConfig }.streamingUpdates;
 
 export const packagesApi = createApi({
   baseQuery: fakeBaseQuery(),
@@ -11,6 +15,22 @@ export const packagesApi = createApi({
         const client = firestoreClientFactory.getClient(table);
         const data = await client.getPackages();
         return { data };
+      },
+      async onCacheEntryAdded(
+        table,
+        { updateCachedData, cacheDataLoaded, cacheEntryRemoved },
+      ) {
+        if (streamingUpdates) {
+          await cacheDataLoaded;
+
+          const client = firestoreClientFactory.getClient(table);
+          const unsubscribe = client.streamPackages((packages) =>
+            updateCachedData(() => packages),
+          );
+
+          await cacheEntryRemoved;
+          unsubscribe();
+        }
       },
       providesTags: ["packages"],
     }),
