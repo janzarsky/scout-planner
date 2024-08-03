c88908bf86a9c1b0cc7b0a919d503a2d7234942b
Jan Zarsky <jan.zarsky@gmail.com>
2024-08-03 13:16:03 +0200

    Add streaming updates to settings

--- src/store/settingsApi.js
+++ src/store/settingsApi.js
@@ -1,5 +1,9 @@
 import { createApi, fakeBaseQuery } from "@reduxjs/toolkit/query/react";
 import { firestoreClientFactory } from "../FirestoreClient";
+import config from "../config.json";
+import localConfig from "../config.local.json";
+
+const streamingUpdates = { ...config, ...localConfig }.streamingUpdates;
 
 export const DEFAULT_TIME_STEP = 15 * 60 * 1000;
 export const DEFAULT_WIDTH = 100;
@@ -24,6 +28,22 @@ export const settingsApi = createApi({
         return {
           data: addDefaults(data),
         };
+      },
+      async onCacheEntryAdded(
+        table,
+        { updateCachedData, cacheDataLoaded, cacheEntryRemoved },
+      ) {
+        if (streamingUpdates) {
+          await cacheDataLoaded;
+
+          const client = firestoreClientFactory.getClient(table);
+          const unsubscribe = client.streamTimetable((timetable) =>
+            updateCachedData(() => addDefaults(timetable)),
+          );
+
+          await cacheEntryRemoved;
+          unsubscribe();
+        }
       },
       providesTags: ["settings"],
     }),
