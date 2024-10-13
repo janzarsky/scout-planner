94e8c9117c08ef742a89ea7c82e085bda460c3cf
Jan Zarsky <jan.zarsky@gmail.com>
2024-10-13 12:51:15 +0200

    Use flat config for ESLint

---
+++ eslint.config.mjs
@@ -0,0 +1,50 @@
+import react from "eslint-plugin-react";
+import cypress from "eslint-plugin-cypress";
+import globals from "globals";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+
+export default [
+  ...compat.extends(
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:cypress/recommended",
+  ),
+  {
+    plugins: {
+      react,
+      cypress,
+    },
+
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+      },
+
+      ecmaVersion: "latest",
+      sourceType: "module",
+    },
+
+    rules: {},
+  },
+  {
+    files: ["src/**/*.js", "src/**/*.jsx"],
+
+    rules: {
+      "react/prop-types": "off",
+    },
+  },
+  {
+    files: ["cypress/**/*.js", "cypress/**/*.jsx"],
+  },
+];
