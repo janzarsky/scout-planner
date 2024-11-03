d69c65a04032766c8436771efa473f4763e9737d
Vojta Staněk <stanekv01@gmail.com>
2024-11-03 16:52:38 +0100

    Fix eslint for TimetableV2

--- src/components/TimetableV2.tsx
+++ src/components/TimetableV2.tsx
@@ -1,4 +1,4 @@
-import React, { Fragment, memo, useCallback, useMemo, useRef, useState } from 'react';
+import React, { Fragment, useCallback, useMemo, useRef, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { useGetGroupsQuery } from '../store/groupsApi';
 import { useGetPackagesQuery } from '../store/packagesApi';
@@ -48,24 +48,18 @@ interface Person {
   name: string;
 }
 
-interface Range {
-  _id: string;
-  name: string;
-}
-
 type Violation = { msg: string, program: string, satisied?: boolean };
 type Violations = Map<string, Violation[]>;
 
 export default function Timetable({
   violations,
-  timeProvider = null,
   printView = false,
 }: {
   violations: Violations;
   timeProvider: null | (() => number);
   printView: boolean;
 }) {
-  const { table, userLevel } = useSelector<any, any>((state) => state.auth);
+  const { userLevel } = useSelector<any, any>((state) => state.auth);
   return <ComposeSchedule editable={userLevel >= level.EDIT && !printView} violations={violations} />;
 }
 
@@ -173,128 +167,126 @@ interface SegmentBoxProps {
   violations: Violation[];
 }
 
-const SegmentBox = memo<SegmentBoxProps>(
-  ({ segment, earliestTime, latestTime, isHovering, isDragged = false, setHovering, dayIndex, onDragStart, onDragEnd, onClick, onContextMenu, editable, isHighlighted, violations }) => {
-    const program = segment.plannable
-    const dayLength = earliestTime.until(latestTime)
-    const setStartTime = segment.start ?? earliestTime
-    const startTime = Temporal.PlainTime.compare(setStartTime, earliestTime) < 0 ? earliestTime : setStartTime
-    const startOffset = earliestTime.until(startTime).total({ unit: 'seconds' }) / dayLength.total({ unit: 'seconds' })
-    const setEndTime = segment.end ?? latestTime
-    const endTime = Temporal.PlainTime.compare(setEndTime, latestTime) > 0 ? latestTime : setEndTime
-    const duration = startTime.until(endTime)
-    const relativeDuration = duration.total({ unit: 'seconds' }) / dayLength.total({ unit: 'seconds' })
+function SegmentBox({ segment, earliestTime, latestTime, isHovering, isDragged = false, setHovering, dayIndex, onDragStart, onDragEnd, onClick, onContextMenu, editable, isHighlighted, violations }: SegmentBoxProps) {
+  const program = segment.plannable
+  const dayLength = earliestTime.until(latestTime)
+  const setStartTime = segment.start ?? earliestTime
+  const startTime = Temporal.PlainTime.compare(setStartTime, earliestTime) < 0 ? earliestTime : setStartTime
+  const startOffset = earliestTime.until(startTime).total({ unit: 'seconds' }) / dayLength.total({ unit: 'seconds' })
+  const setEndTime = segment.end ?? latestTime
+  const endTime = Temporal.PlainTime.compare(setEndTime, latestTime) > 0 ? latestTime : setEndTime
+  const duration = startTime.until(endTime)
+  const relativeDuration = duration.total({ unit: 'seconds' }) / dayLength.total({ unit: 'seconds' })
 
-    const [clickWidthRatio, setClickWidthRatio] = useState(0)
+  const [clickWidthRatio, setClickWidthRatio] = useState(0)
 
-    if (dayIndex < 0) {
-      return null
-    }
+  if (dayIndex < 0) {
+    return null
+  }
 
-    const pkgs = usePkgs();
-    const pkg = pkgs.find((pkg) => pkg._id === segment.plannable.pkg);
-    const color = pkg?.color ?? DEFAULT_PROGRAM_COLOR;
-    const isDark = color !== null && isColorDark(color);
-    const people = usePeople();
-    const ownerNames = segment.plannable.people.map((person) => (people.find((p) => p._id === person.person))?.name ?? person.person).join(', ');
-    return (
+  const pkgs = usePkgs();
+  const pkg = pkgs.find((pkg) => pkg._id === segment.plannable.pkg);
+  const color = pkg?.color ?? DEFAULT_PROGRAM_COLOR;
+  const isDark = color !== null && isColorDark(color);
+  const people = usePeople();
+  const ownerNames = segment.plannable.people.map((person) => (people.find((p) => p._id === person.person))?.name ?? person.person).join(', ');
+  return (
+    <div
+      className={[
+        'scheduleTable__plannable',
+        isHovering != null && 'scheduleTable__plannable--hovering',
+        isDragged && 'scheduleTable__plannable--dragged',
+        (segment.start?.equals(startTime)) && 'scheduleTable__plannable--start',
+        (segment.end?.equals(endTime)) && 'scheduleTable__plannable--end',
+        isDark && 'scheduleTable__plannable--dark',
+        isHighlighted && 'scheduleTable__plannable--highlighted',
+        isHighlighted === false && 'scheduleTable__plannable--notHighlighted',
+        violations.length > 0 && 'scheduleTable__plannable--violated',
+      ].filter(Boolean).join(' ')}
+      style={{
+        '--segment-day': dayIndex,
+        '--segment-start': startOffset,
+        '--segment-duration': relativeDuration,
+        '--segment-group-first': segment.atendeeGroups[0],
+        '--segment-groups-count': segment.atendeeGroups.length,
+        '--segment-color': color,
+      } as any}
+
+      onMouseEnter={(e) => setHovering({ clientX: e.clientX, clientY: e.clientY })}
+      onMouseMove={(e) => setHovering({ clientX: e.clientX, clientY: e.clientY })}
+      onMouseLeave={() => setHovering(null)}
+
+      {...(editable ? {
+        draggable: true,
+        onDragStart: (e) => {
+          e.dataTransfer.setData(MIME_TYPE, program._id as string)
+          e.dataTransfer.effectAllowed = "move"
+
+          const canvas = document.createElement("canvas");
+          canvas.width = canvas.height = 0;
+          e.dataTransfer.setDragImage(canvas, 25, 25);
+
+          onDragStart(clickWidthRatio)
+        },
+        onDragEnd: () => {
+          onDragEnd()
+        },
+        onMouseDown: (e) => {
+          if (segment.start !== null && segment.end !== null) {
+            setClickWidthRatio(e.nativeEvent.offsetX / e.currentTarget.offsetWidth)
+          } else {
+            setClickWidthRatio(0)
+          }
+        },
+      } : {})}
+
+      onClick={(e) => {
+        e.stopPropagation()
+        onClick()
+      }}
+      onContextMenu={onContextMenu ? e => {
+        e.preventDefault()
+        e.stopPropagation()
+        onContextMenu?.(e.clientX, e.clientY)
+      } : undefined}
+    >
       <div
-        className={[
-          'scheduleTable__plannable',
-          isHovering != null && 'scheduleTable__plannable--hovering',
-          isDragged && 'scheduleTable__plannable--dragged',
-          (segment.start?.equals(startTime)) && 'scheduleTable__plannable--start',
-          (segment.end?.equals(endTime)) && 'scheduleTable__plannable--end',
-          isDark && 'scheduleTable__plannable--dark',
-          isHighlighted && 'scheduleTable__plannable--highlighted',
-          isHighlighted === false && 'scheduleTable__plannable--notHighlighted',
-          violations.length > 0 && 'scheduleTable__plannable--violated',
-        ].filter(Boolean).join(' ')}
-        style={{
-          '--segment-day': dayIndex,
-          '--segment-start': startOffset,
-          '--segment-duration': relativeDuration,
-          '--segment-group-first': segment.atendeeGroups[0],
-          '--segment-groups-count': segment.atendeeGroups.length,
-          '--segment-color': color,
-        } as any}
-
-        onMouseEnter={(e) => setHovering({ clientX: e.clientX, clientY: e.clientY })}
-        onMouseMove={(e) => setHovering({ clientX: e.clientX, clientY: e.clientY })}
-        onMouseLeave={() => setHovering(null)}
-
-        {...(editable ? {
-          draggable: true,
-          onDragStart: (e) => {
-            e.dataTransfer.setData(MIME_TYPE, program._id as string)
-            e.dataTransfer.effectAllowed = "move"
-
-            const canvas = document.createElement("canvas");
-            canvas.width = canvas.height = 0;
-            e.dataTransfer.setDragImage(canvas, 25, 25);
-
-            onDragStart(clickWidthRatio)
-          },
-          onDragEnd: () => {
-            onDragEnd()
-          },
-          onMouseDown: (e) => {
-            if (segment.start !== null && segment.end !== null) {
-              setClickWidthRatio(e.nativeEvent.offsetX / e.currentTarget.offsetWidth)
-            } else {
-              setClickWidthRatio(0)
-            }
-          },
-        } : {})}
-
-        onClick={(e) => {
-          e.stopPropagation()
-          onClick()
-        }}
-        onContextMenu={onContextMenu ? e => {
-          e.preventDefault()
-          e.stopPropagation()
-          onContextMenu?.(e.clientX, e.clientY)
-        } : undefined}
+        className="scheduleTable__plannableTitle"
       >
-        <div
-          className="scheduleTable__plannableTitle"
-        >
-          {program.locked && <i className="fa fa-lock me-1" />}
-          {violations.length > 0 && <i className="fa fa-exclamation-triangle me-1" />}
-          {program.title}
-        </div>
-        {ownerNames && <div className="scheduleTable__plannableOwners">{ownerNames}</div>}
-        {(isHovering != null && !isDragged) && (
-          <div
-            className="scheduleTable__plannableExpansion"
-            style={{
-              '--position-x': `${isHovering.clientX}px`,
-              '--position-y': `${isHovering.clientY}px`,
-            } as any}
-          >
-            <div
-              className="scheduleTable__plannableTitle"
-            >
-              {program.title}
-            </div>
-            {violations.length > 0 && (
-              <div className="scheduleTable__plannableOwners">
-                <i className="fa fa-exclamation-triangle me-1" />
-                {violations.map((violation) => violation.msg).join(', ')}
-              </div>
-            )}
-            <div className="scheduleTable__plannableOwners">{pkg?.name ?? "Bez balíčku"}</div>
-            {ownerNames && <div className="scheduleTable__plannableOwners">{ownerNames}</div>}
-            <div className="scheduleTable__plannableOwners">
-              {startTime.toLocaleString('cs-CZ', { hour: 'numeric', minute: '2-digit' })} - {endTime.toLocaleString('cs-CZ', { hour: 'numeric', minute: '2-digit' })} ({duration.total({ unit: 'minute' })} min)
-            </div>
-          </div>
-        )}
+        {program.locked && <i className="fa fa-lock me-1" />}
+        {violations.length > 0 && <i className="fa fa-exclamation-triangle me-1" />}
+        {program.title}
       </div>
-    );
-  },
-)
+      {ownerNames && <div className="scheduleTable__plannableOwners">{ownerNames}</div>}
+      {(isHovering != null && !isDragged) && (
+        <div
+          className="scheduleTable__plannableExpansion"
+          style={{
+            '--position-x': `${isHovering.clientX}px`,
+            '--position-y': `${isHovering.clientY}px`,
+          } as any}
+        >
+          <div
+            className="scheduleTable__plannableTitle"
+          >
+            {program.title}
+          </div>
+          {violations.length > 0 && (
+            <div className="scheduleTable__plannableOwners">
+              <i className="fa fa-exclamation-triangle me-1" />
+              {violations.map((violation) => violation.msg).join(', ')}
+            </div>
+          )}
+          <div className="scheduleTable__plannableOwners">{pkg?.name ?? "Bez balíčku"}</div>
+          {ownerNames && <div className="scheduleTable__plannableOwners">{ownerNames}</div>}
+          <div className="scheduleTable__plannableOwners">
+            {startTime.toLocaleString('cs-CZ', { hour: 'numeric', minute: '2-digit' })} - {endTime.toLocaleString('cs-CZ', { hour: 'numeric', minute: '2-digit' })} ({duration.total({ unit: 'minute' })} min)
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
 
 interface TimeLabelsProps {
   earliestTime: Temporal.PlainTime;
@@ -302,7 +294,7 @@ interface TimeLabelsProps {
   dayLength: Temporal.Duration;
 }
 
-const TimeLabels = memo<TimeLabelsProps>(({ earliestTime, timeLabels, dayLength }) => {
+function TimeLabels({ earliestTime, timeLabels, dayLength }: TimeLabelsProps) {
   return (
     <>
       <div className="scheduleTable__timeLabels">
@@ -333,14 +325,14 @@ const TimeLabels = memo<TimeLabelsProps>(({ earliestTime, timeLabels, dayLength 
       })}
     </>
   )
-})
+}
 
 interface DataLabelsProps {
   dates: Temporal.PlainDate[];
   virtualGroupShown: boolean;
 }
 
-const DataLabels = memo<DataLabelsProps>(({ dates, virtualGroupShown }) => {
+function DataLabels({ dates, virtualGroupShown }: DataLabelsProps) {
   const groups = useGroups();
   const addGroupOffset = virtualGroupShown ? 1 : 0;
 
@@ -382,7 +374,7 @@ const DataLabels = memo<DataLabelsProps>(({ dates, virtualGroupShown }) => {
       ))}
     </>
   )
-})
+}
 
 interface ComposeScheduleProps {
   editable: boolean;
@@ -708,7 +700,7 @@ export const ComposeSchedule = ({ editable, violations }: ComposeScheduleProps) 
               dayLength={dayLength}
             />
 
-            {[...segments].map((segment, index) => {
+            {[...segments].map((segment) => {
               return (
                 <SegmentBox
                   key={segment.plannable._id}
@@ -753,7 +745,7 @@ export const ComposeSchedule = ({ editable, violations }: ComposeScheduleProps) 
                 earliestTime={earliestTime}
                 latestTime={latestTime}
                 isHovering={null}
-                setHovering={isHovering => { }}
+                setHovering={() => {}}
                 dayIndex={dates.findIndex(it => it.equals(segment.date))}
                 onDragStart={(widthRatio) => {
                   setDraggingPlannable({
