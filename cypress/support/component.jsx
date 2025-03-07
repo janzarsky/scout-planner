865ac76e4263e0c60a73edd34252c0da0730d79f
Jan Žárský <jan.zarsky@gmail.com>
2025-03-07 10:49:35 +0100

    Update react and cypress (#812)

--- cypress/support/component.jsx
+++ cypress/support/component.jsx
@@ -1,6 +1,6 @@
 import "./commands";
 import React from "react";
-import { mount } from "cypress/react18";
+import { mount } from "cypress/react";
 import { Provider } from "react-redux";
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
