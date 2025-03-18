4efc0ce36f6e5fc87267697c3072cb153c3f3f45
Jan Zarsky <jan.zarsky@gmail.com>
2025-03-18 08:16:15 +0100

    Separate test config for app and functions

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+/// <reference types="vitest/config" />
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
@@ -5,5 +6,8 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
+  },
+  test: {
+    include: ["src/**/*.test.js", "src/**/*.test.ts"],
   },
 });
