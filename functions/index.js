cd6d28d8e9817b7bad1adf4af618461e375a5fab
Jan Žárský <jan.zarsky@gmail.com>
2025-03-18 14:17:42 +0100

    Integrate firestore in cloud functions (#843)
    * Parse options
    
    * Remove old workflow
    
    * Add firestore

--- functions/index.js
+++ functions/index.js
@@ -1,21 +1,39 @@
 import { http } from "@google-cloud/functions-framework";
-import admin from "firebase-admin";
-
-admin.initializeApp();
+import { initializeApp } from "firebase-admin/app";
+import { getAuth } from "firebase-admin/auth";
+import { getFirestore } from "firebase-admin/firestore";
 
 http("clone-timetable", cloneTimetable);
 
+initializeApp();
+
 async function cloneTimetable(req, res) {
+  let email = null;
   try {
-    const email = await getIdentity(req);
-
+    email = await getIdentity(req);
     console.debug("Got customer email: " + email);
-
-    throw new Error("Not implemented");
   } catch (error) {
     console.error(error);
     res.status(401).send("Unauthorized");
   }
+
+  let options = null;
+  try {
+    options = getOptions(req);
+    console.debug("Got options: " + options);
+  } catch (error) {
+    console.error(error);
+    res.status(400).send("Invalid parameters");
+  }
+
+  try {
+    await cloneData(options.source);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal server error");
+  }
+
+  res.status(200).send("Successfullly cloned");
 }
 
 async function getIdentity(req) {
@@ -26,11 +44,31 @@ async function getIdentity(req) {
   }
   const idToken = authorizationHeader.split("Bearer ")[1];
 
-  const token = await admin.auth().verifyIdToken(idToken);
+  const token = await getAuth().verifyIdToken(idToken);
 
   return token.email;
 }
 
+function getOptions(req) {
+  const source = req.query.source;
+  if (!source) throw new Error("Invalid parameters");
+
+  const destination = req.query.destination;
+  if (!destination) throw new Error("Invalid parameters");
+
+  return { source, destination };
+}
+
+async function cloneData(source) {
+  const db = getFirestore();
+  const sourceTimetable = await db.doc("timetables/" + source).get();
+
+  console.log(sourceTimetable);
+
+  throw new Error("Not implemented");
+}
+
 export const testing = {
   getIdentity,
+  getOptions,
 };
