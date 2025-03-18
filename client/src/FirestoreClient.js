863f56fe891e4aaf2b133e28547df534abef404b
Jan Zarsky <jan.zarsky@gmail.com>
2025-03-18 14:40:17 +0100

    Inject db and auth to client

--- client/src/FirestoreClient.js
+++ client/src/FirestoreClient.js
@@ -1,7 +1,12 @@
+import { getFirestore } from "firebase/firestore";
 import { Client } from "./Client";
+import { getAuth } from "firebase/auth";
 
 export const firestoreClientFactory = {
   getClient(table) {
-    return new Client(table);
+    const db = getFirestore();
+    const auth = getAuth();
+
+    return new Client(table, db, auth);
   },
 };
