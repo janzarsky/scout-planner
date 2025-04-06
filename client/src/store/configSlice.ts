87c6e2746ab8e67da835688036b0b9404cf56d0f
Jan Zarsky <jan.zarsky@gmail.com>
2025-04-06 20:19:29 +0200

    Add group lock settings feature flag

--- client/src/store/configSlice.ts
+++ client/src/store/configSlice.ts
@@ -10,6 +10,8 @@ const defaultConfig = {
       .VITE_REACT_APP_TIMETABLE_LAYOUT_VERSION_SWITCHING_ENABLED === "true",
   newTray: import.meta.env.VITE_REACT_APP_NEW_TRAY === "true",
   dropIntoBlock: import.meta.env.VITE_REACT_APP_DROP_INTO_BLOCK === "true",
+  groupLockSettings:
+    import.meta.env.VITE_REACT_APP_GROUP_LOCK_SETTINGS === "true",
 };
 
 type Config = typeof defaultConfig;
