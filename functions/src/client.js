68f25282431942f0b814614c964240de4002f9e0
Jan Žárský <jan.zarsky@gmail.com>
2025-05-07 11:05:22 +0200

    Import function (#902)
    * Add functions client
    
    * Load data for import
    
    * Load data in parallel
    
    * Check access level for both tables

---
+++ functions/src/client.js
@@ -0,0 +1,161 @@
+import { level } from "@scout-planner/common/level";
+
+export class Client {
+  constructor(table, db) {
+    [
+      ["programs", "Program", "Programs"],
+      ["packages", "Package", "Packages"],
+      ["rules", "Rule", "Rules"],
+      ["groups", "Group", "Groups"],
+      ["ranges", "Range", "Ranges"],
+      ["users", "User", "Users"],
+      ["people", "Person", "People"],
+    ].forEach(([path, nameSingular, namePlural]) => {
+      this[`get${namePlural}`] = () => this.getAll(path);
+      this[`get${nameSingular}`] = (id) => this.get(path, id);
+      this[`add${nameSingular}`] = (data) => this.post(path, data);
+      this[`update${nameSingular}`] = (data) => this.put(path, data);
+      this[`delete${nameSingular}`] = (id) => this.remove(path, id);
+    });
+
+    this.db = db;
+    this.table = table;
+  }
+
+  async getAccessLevel(user) {
+    try {
+      const publicLevel = await this.db
+        .doc(`timetables/${this.table}`)
+        .get()
+        .then(
+          (table) =>
+            table.exists && table.data().publicLevel !== undefined
+              ? table.data().publicLevel
+              : level.ADMIN,
+          () => level.NONE,
+        );
+
+      if (user) {
+        const userLevel = await this.db
+          .doc(`timetables/${this.table}/users/${user}`)
+          .get()
+          .then(
+            (user) =>
+              user.exists && user.data().level !== undefined
+                ? user.data().level
+                : level.NONE,
+            () => level.NONE,
+          );
+
+        return userLevel > publicLevel ? userLevel : publicLevel;
+      }
+
+      return publicLevel;
+    } catch (e) {
+      throw new Error(e.message);
+    }
+  }
+
+  async getPublicLevel() {
+    return await this.db
+      .doc(`timetables/${this.table}`)
+      .get()
+      .then(
+        (table) =>
+          table.exists && typeof table.data().publicLevel === "number"
+            ? table.data().publicLevel
+            : level.ADMIN,
+        () => level.NONE,
+      );
+  }
+
+  async setPublicLevel(level) {
+    try {
+      const timetableDoc = this.db.doc(`timetables/${this.table}`);
+      const snapshot = await timetableDoc.get();
+
+      if (snapshot.exists) await timetableDoc.update({ publicLevel: level });
+      else await timetableDoc.set({ publicLevel: level });
+
+      return level;
+    } catch (e) {
+      throw new Error(e.message);
+    }
+  }
+
+  async getTimetable() {
+    try {
+      const snapshot = await this.db.doc(`timetables/${this.table}`).get();
+      return snapshot.data();
+    } catch (e) {
+      throw new Error(e.message);
+    }
+  }
+
+  async updateTimetable(data) {
+    try {
+      const timetableDoc = this.db.doc(`timetables/${this.table}`);
+      const snapshot = await timetableDoc.get();
+
+      if (snapshot.exists) await timetableDoc.update(data);
+      else await timetableDoc.set(data);
+    } catch (e) {
+      throw new Error(e.message);
+    }
+  }
+
+  async get(coll, id) {
+    try {
+      const snapshot = await this.db
+        .doc(`timetables/${this.table}/${coll}/${id}`)
+        .get();
+      return { ...snapshot.data(), _id: snapshot.id };
+    } catch (e) {
+      throw new Error(e.message);
+    }
+  }
+
+  async getAll(coll) {
+    try {
+      const q = this.db.collection(`timetables/${this.table}/${coll}`);
+      const snapshot = await q.get();
+      return snapshot.docs.map((doc) => ({
+        ...doc.data(),
+        _id: doc.id,
+      }));
+    } catch (e) {
+      throw new Error(e.message);
+    }
+  }
+
+  async post(coll, data) {
+    try {
+      const docRef = await this.db
+        .collection(`timetables/${this.table}/${coll}`)
+        .add(data);
+      return { ...data, _id: docRef.id };
+    } catch (e) {
+      throw new Error(e.message);
+    }
+  }
+
+  async put(coll, data) {
+    try {
+      await this.db
+        .doc(`timetables/${this.table}/${coll}/${data._id}`)
+        .set(data);
+
+      return data;
+    } catch (e) {
+      throw new Error(e.message);
+    }
+  }
+
+  async remove(coll, id) {
+    try {
+      await this.db.doc(`timetables/${this.table}/${coll}/${id}`).delete();
+    } catch (e) {
+      throw new Error(e.message);
+    }
+  }
+}
--- functions/src/index.js
+++ functions/src/index.js
@@ -2,38 +2,75 @@ import { http } from "@google-cloud/functions-framework";
 import { initializeApp } from "firebase-admin/app";
 import { getAuth } from "firebase-admin/auth";
 import { getFirestore } from "firebase-admin/firestore";
+import { importData } from "@scout-planner/common/importer";
+import { Client } from "./client";
 import { level } from "@scout-planner/common/level";
 
 http("clone-timetable", cloneTimetable);
 
 initializeApp();
 
+const db = getFirestore();
+db.settings({ ignoreUndefinedProperties: true });
+
 async function cloneTimetable(req, res) {
   let email = null;
   try {
     email = await getIdentity(req);
-    console.debug("Got customer email: " + email);
+    console.debug(`Got customer email: "${email}"`);
   } catch (error) {
     console.error(error);
     res.status(401).send("Unauthorized");
+    return;
   }
 
   let options = null;
   try {
     options = getOptions(req);
-    console.debug("Got options: " + options);
+    console.debug(
+      `Got source: "${options.source}" and destination: "${options.destination}"`,
+    );
   } catch (error) {
     console.error(error);
     res.status(400).send("Invalid parameters");
+    return;
   }
 
   try {
-    await cloneData(options.source);
+    const accessLevel = await getAccessLevel(db, options.source, email);
+    console.debug(`Got access level to source table: ${accessLevel}`);
+    if (accessLevel < level.VIEW) {
+      res.status(403).send("Access to source table forbidden");
+      return;
+    }
   } catch (error) {
     console.error(error);
     res.status(500).send("Internal server error");
+    return;
   }
 
+  try {
+    const accessLevel = await getAccessLevel(db, options.destination, email);
+    console.debug(`Got access level to destination table: ${accessLevel}`);
+    if (accessLevel < level.ADMIN) {
+      res.status(403).send("Access to destination table forbidden");
+      return;
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal server error");
+    return;
+  }
+
+  try {
+    await cloneData(db, options.source, options.destination);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal server error");
+    return;
+  }
+
+  console.debug("Successfully cloned");
   res.status(200).send("Successfullly cloned");
 }
 
@@ -60,16 +97,64 @@ function getOptions(req) {
   return { source, destination };
 }
 
-async function cloneData(source) {
-  const db = getFirestore();
-  const sourceTimetable = await db.doc("timetables/" + source).get();
+async function getAccessLevel(db, table, user) {
+  const client = new Client(table, db);
+  return await client.getAccessLevel(user);
+}
 
-  console.log(sourceTimetable);
+async function cloneData(db, source, destination) {
+  const sourceClient = new Client(source, db);
+  const data = await loadData(sourceClient);
 
-  // FIXME: test of level
-  console.log(level.ADMIN);
+  const destinationClient = new Client(destination, db);
+  await importData(data, destinationClient);
+}
 
-  throw new Error("Not implemented");
+async function loadData(client) {
+  const [groups, ranges, pkgs, rules, users, people, programs, timetable] =
+    await Promise.all([
+      client.getGroups(),
+      client.getRanges(),
+      client.getPackages(),
+      client.getRules(),
+      client.getUsers(),
+      client.getPeople(),
+      client.getPrograms(),
+      client.getTimetable(),
+    ]);
+
+  return {
+    groups,
+    ranges,
+    pkgs,
+    rules,
+    users,
+    people,
+    programs: programs.filter((p) => !p.deleted),
+    settings: addSettingsDefaults(timetable),
+    timetable: addTimetableDefaults(timetable),
+  };
+}
+
+// TODO deduplicate these helper functions
+function addTimetableDefaults(data) {
+  return {
+    title: null,
+    layoutVersion: "v1",
+    ...data,
+  };
+}
+
+const DEFAULT_TIME_STEP = 15 * 60 * 1000;
+const DEFAULT_WIDTH = 100;
+
+function addSettingsDefaults(data) {
+  return {
+    timeStep: DEFAULT_TIME_STEP,
+    width: DEFAULT_WIDTH,
+    groupLock: false,
+    ...(data && data.settings ? data.settings : {}),
+  };
 }
 
 export const testing = {
