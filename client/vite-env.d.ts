754c7f4d0e28890399f89cf6866f0266c17fbb17
Jan Zarsky <jan.zarsky@gmail.com>
2025-05-09 21:03:30 +0200

    Add env variable for functions base URL

--- client/vite-env.d.ts
+++ client/vite-env.d.ts
@@ -2,6 +2,7 @@ interface ImportMetaEnv {
   readonly VITE_REACT_APP_API_KEY: string;
   readonly VITE_REACT_APP_AUTH_DOMAIN: string;
   readonly VITE_REACT_APP_PROJECT_ID: string;
+  readonly VITE_REACT_APP_FUNCTIONS_BASE_URL: string;
   readonly VITE_REACT_APP_TIMETABLE_LAYOUT_VERSION_SWITCHING_ENABLED: string;
   readonly VITE_REACT_APP_NEW_TRAY: string;
   readonly VITE_REACT_APP_DROP_INTO_BLOCK: string;
