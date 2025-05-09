c5de736e720514a35846a7747e5d7f81686effc6
Jan Zarsky <jan.zarsky@gmail.com>
2025-05-09 20:46:39 +0200

    Allow authorization header in CORS

--- functions/src/index.js
+++ functions/src/index.js
@@ -19,7 +19,7 @@ async function cloneTimetable(req, res) {
 
   if (req.method === "OPTIONS") {
     res.set("Access-Control-Allow-Methods", "GET");
-    res.set("Access-Control-Allow-Headers", "Content-Type");
+    res.set("Access-Control-Allow-Headers", "Content-Type, Authorization");
     res.set("Access-Control-Max-Age", "3600");
     res.status(204).send("");
     return;
