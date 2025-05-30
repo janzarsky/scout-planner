e0c098e64f4e4bbff56b5c14933ff18e8b8d946e
Jan Žárský <jan.zarsky@gmail.com>
2025-05-30 12:12:30 +0200

    Add shift API (#961)
    * Add shift API
    
    * Add error messages

---
+++ client/src/store/shiftApi.js
@@ -0,0 +1,37 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const shiftApi = createApi({
+  baseQuery: fetchBaseQuery({
+    baseUrl: import.meta.env.VITE_REACT_APP_FUNCTIONS_BASE_URL,
+  }),
+  reducerPath: "shiftApi",
+  endpoints: (builder) => ({
+    shift: builder.mutation({
+      query: ({ source, offset, token }) => ({
+        url: `/shift-timetable`,
+        params: { source, offset },
+        method: "POST",
+        headers: { Authorization: `Bearer ${token}` },
+      }),
+      transformErrorResponse: (response) => ({
+        message: translateError(response),
+      }),
+    }),
+  }),
+});
+
+const msgs = {
+  unauthorized: "Pro posun data se prosím přihlaste",
+  forbidden: "Nemáte přístup k cílovému harmonogramu",
+  generic: "Během posunu data nastala chyba",
+};
+
+function translateError(response) {
+  if (response?.status === 401) return msgs.unauthorized;
+
+  if (response?.status === 403) return msgs.forbidden;
+
+  return msgs.generic;
+}
+
+export const { useShiftMutation } = shiftApi;
