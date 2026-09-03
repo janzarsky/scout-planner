67a93ce42f217fed9f5391751ebaf8e2d2d4f704
Jan Žárský <jan.zarsky@gmail.com>
2026-09-03 19:28:47 +0200

    Fix app initialization (#1171)
    Use explicit app and auth variables

--- create/src/index.js
+++ create/src/index.js
@@ -1,15 +1,19 @@
 import { http } from "@google-cloud/functions-framework";
 import { initializeApp } from "firebase-admin/app";
+import { getAuth } from "firebase-admin/auth";
 import { corsMiddleware } from "@scout-planner/common/corsMiddleware";
-import { authMiddleware } from "@scout-planner/common/authMiddleware";
+import { createAuthMiddleware } from "@scout-planner/common/authMiddleware";
+
+const app = initializeApp();
+const auth = getAuth(app);
+
+const authMiddleware = createAuthMiddleware(auth);
 
 http("create-timetable", async (req, res) =>
   corsMiddleware(["POST"])(req, res, async () =>
     authMiddleware(req, res, async () => createTimetable(req, res)),
   ),
 );
-
-initializeApp();
 
 async function createTimetable(req, res) {
   console.error("Not implemented");
